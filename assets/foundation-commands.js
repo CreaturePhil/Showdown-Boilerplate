f8580e634dee62e929c00c4456b0a893c993b0d7
Phil <birkal@outlook.com>
2014-05-04 17:40:55 -0700

    Sets up foundation commands

---
+++ assets/foundation-commands.js
@@ -0,0 +1,17 @@
+/**
+ * Foundation commands
+ * Created by CreaturePhil - https://github.com/CreaturePhil
+ *
+ * These are custom commands for the server and most of them if not all
+ * are not made by CreaturePhil. They come from other servers.
+ *
+ * Command categories: 
+ *
+ * @license MIT license
+ */
+
+var foundationCommands = exports.foundationCommands = {
+
+};
+
+Object.merge(CommandParser.commands, foundationCommands);
