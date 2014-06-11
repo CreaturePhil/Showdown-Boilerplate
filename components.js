68639768dd5d97fc4599d0e8846e3a0da7f082e9
Phil <birkal@outlook.com>
2014-06-10 17:26:24 -0700

    Improve /sudo command
    Fixes issue#3 where forgetting a comma or user not found crashes the
    server and sudo command now works for /me command. In addition, commands
    not found will no longer work and commands that uses commas will now work
    such as /givemoney user, amount

--- components.js
+++ components.js
@@ -539,10 +539,25 @@ var components = exports.components = {
 
     sudo: function (target, room, user) {
         if (!user.can('sudo')) return;
-        if (!target) return this.parse('/help sudo');
         var parts = target.split(',');
-        CommandParser.parse(parts[1].trim(), room, Users.get(parts[0]), Users.get(parts[0]).connections[0]);
-        return this.sendReply('You have made ' + parts[0] + ' do ' + parts[1] + '.');
+        if (parts.length < 2) return this.parse('/help sudo');
+        if (parts.length >= 3) parts.push(parts.splice(1, parts.length).join(','));
+        var targetUser = parts[0],
+            cmd = parts[1].trim().toLowerCase(),
+            commands = Object.keys(CommandParser.commands).join(' ').toString(),
+            spaceIndex = cmd.indexOf(' '),
+            targetCmd = cmd;
+
+        if (spaceIndex > 0) targetCmd = targetCmd.substr(1, spaceIndex - 1);
+
+        if (!Users.get(targetUser)) return this.sendReply('User ' + targetUser + ' not found.');
+        if (commands.indexOf(targetCmd.substring(1, targetCmd.length)) < 0 || targetCmd === '') return this.sendReply('Not a valid command.');
+        if (cmd.match(/\/me/)) {
+            if (cmd.match(/\/me./)) return this.parse('/control ' + targetUser + ', say, ' + cmd);
+            return this.sendReply('You must put a target to make a user use /me.');
+        }
+        CommandParser.parse(cmd, room, Users.get(targetUser), Users.get(targetUser).connections[0]);
+        this.sendReply('You have made ' + targetUser + ' do ' + cmd + '.');
     },
 
     poll: function (target, room, user) {
