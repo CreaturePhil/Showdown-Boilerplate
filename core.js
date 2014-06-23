f583b3375721c87a0faefbedb0e63883f0687331
Phil <birkal@outlook.com>
2014-06-22 17:15:35 -0700

    Add avatarurl

--- core.js
+++ core.js
@@ -68,16 +68,18 @@ var core = exports.core = {
 
         color: '#2ECC40',
 
+        avatarurl: 'http://107.161.19.15:8000',
+
         avatar: function (online, user, img) {
             if (online === true) {
                 if (typeof (img) === typeof ('')) {
-                    return '<img src="http://107.161.19.15:8000/avatars/' + img + '" width="80" height="80" align="left">';
+                    return '<img src="' + this.avatarurl + '/avatars/' + img + '" width="80" height="80" align="left">';
                 }
                 return '<img src="http://play.pokemonshowdown.com/sprites/trainers/' + img + '.png" width="80" height="80" align="left">';
             }
             for (var name in Config.customAvatars) {
                 if (user === name) {
-                    return '<img src="http://107.161.19.15:8000/avatars/' + Config.customAvatars[name] + '" width="80" height="80" align="left">';
+                    return '<img src="' + this.avatarurl + '/avatars/' + Config.customAvatars[name] + '" width="80" height="80" align="left">';
                 }
             }
             var trainersprites = [1, 2, 101, 102, 169, 170, 265, 266, 168];
