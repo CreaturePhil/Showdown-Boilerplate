95deb2615f454f4ade0f40fac5b23b0e407ccfea
Phil <birkal@outlook.com>
2015-07-28 13:39:45 -0700

    Setup for mysql

--- database.js
+++ database.js
@@ -1,4 +1,64 @@
+/**
+ * Database file
+ * To easily switch between different database using a common API.
+ *
+ *
+ * methods.read(key, username, callback)
+ *   Reads a key in the database.
+ *
+ *   @param {String} key
+ *   @param {String} user
+ *   @param {Function} callback(err, value)
+ *
+ * methods.write(key, value, user, callback)
+ *   Writes a key to value in the database.
+ *
+ *   @param {String} key
+ *   @param {*} value
+ *   @param {String} user
+ *   @param {Function} callback(err, value)
+ *
+ * methods.total(key, callback)
+ *   Combined value from all rows.
+ *
+ *   @param {String} key
+ *   @param {Function} callback(err, total)
+ *
+ * methods.countUsers(callback)
+ *   Gets how many users there are.
+ *
+ *   @param {Function} callback(err, size)
+ *
+ * methods.sortDesc(key, amount, callback)
+ *   Sort by a key in descending order.
+ *
+ *   @param {String} key
+ *   @param {Number} amount
+ *   @param {Function} callback(err, array)
+ *
+ * methods.get(key, callback)
+ *   Get a key in the database.
+ *
+ *   @param {String} key
+ *   @param {Function} callback(err, key)
+ *
+ * methods.set(key, value, callback)
+ *   Set a key in the database.
+ *
+ *   @param {String} key
+ *   @param {Number} value
+ *   @param {Function} callback(err, newKey)
+ *
+ * methods.users(callback)
+ *   Get the users array in the database.
+ *
+ *   @param {Function} callback(err, users)
+ *
+ * @license MIT license
+ */
+
 var low = require('lowdb');
+var mysql = require('mysql');
 var path = require('path');
 
 var lowFile = path.join(__dirname, 'config/db.json');
@@ -9,27 +69,12 @@ databases.lowdb = function () {
 	var db = low(lowFile);
 	var methods = {};
 
-	/**
-	 * Reads a key in the database.
-	 *
-	 * @param {String} key
-	 * @param {String} user
-	 * @param {Function} callback(err, value)
-	 */
 	methods.read = function (key, username, callback) {
 		var user = db('users').find({username: username});
 		if (!user) return callback(null);
 		callback(null, user[key]);
 	};
 
-	/**
-	 * Writes a key to value in the database.
-	 *
-	 * @param {String} key
-	 * @param {*} value
-	 * @param {String} user
-	 * @param {Function} callback(err, value)
-	 */
 	methods.write = function (key, value, username, callback) {
 		var user = db('users').find({username: username});
 		if (!user) db('users').push({username: username});
@@ -44,12 +89,6 @@ databases.lowdb = function () {
 		callback(null, val[key]);
 	};
 
-	/**
-	 * Combined value from all rows.
-	 *
-	 * @param {String} key
-	 * @param {Function} callback(err, total)
-	 */
 	methods.total = function (key, callback) {
 		var total = db('users').reduce(function (acc, user) {
 			if (!acc[key] || !user[key]) return {money: acc[key]};
@@ -58,22 +97,10 @@ databases.lowdb = function () {
 		callback(null, total.money);
 	};
 
-	/**
-	 * Gets how many users there are.
-	 *
-	 * @param {Function} callback(err, size)
-	 */
 	methods.countUsers = function (callback) {
 		return callback(null, db('users').size());
 	};
 
-	/**
-	 * Sort by a key in descending order.
-	 *
-	 * @param {String} key
-	 * @param {Number} amount
-	 * @param {Function} callback(err, array)
-	 */
 	methods.sortDesc = function (key, amount, callback) {
 		var value = db('users')
 						.chain()
@@ -87,40 +114,30 @@ databases.lowdb = function () {
 		callback(null, value);
 	};
 
-	/**
-	* Get a key in the database.
-	*
-	* @param {String} key
-	* @param {Function} callback(err, key)
-	*/
 	methods.get = function (key, callback) {
 		if (key === 'users') return callback(new Error('Cannot overwrite users'));
 		callback(null, db.object[key]);
 	};
 
-	/**
-	* Set a key in the database.
-	*
-	* @param {String} key
-	* @param {Number} value
-	* @param {Function} callback(err, newKey)
-	*/
 	methods.set = function (key, value, callback) {
 		if (key === 'users') return callback(new Error('Cannot overwrite users'));
 		db.object[key] = value;
 		callback(null, db.object[key]);
 	};
 
-	/**
-	* Get the users array in the database.
-	*
-	* @param {Function} callback(err, users)
-	*/
 	methods.users = function (callback) {
 		callback(null, db('users').value());
 	};
 
 	return methods;
+};
+
+methods.mysql = function () {
+		var methods = {};
+		var connection = mysql.createConnection(Config.mysql);
+		connection.connect();
+
+		return methods;
 };
 
 function Database (database) {
