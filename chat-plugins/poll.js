caa992122f14becb8ae0e1da4df094ed2286c772
naten2006 <naten1995@gmail.com>
2015-08-10 17:21:20 -0400

    Update poll.js

--- chat-plugins/poll.js
+++ chat-plugins/poll.js
@@ -123,6 +123,20 @@ exports.commands = {
 	tierspoll: 'tierpoll',
 	tierpoll: function (target, room, user) {
 		if (!this.can('broadcast', null, room)) return false;
+		this.parse('/poll Tier for the next tournament?, Random Battles, Double Random Battles, Triple Random Battles, Gen [5] Random Battles, Gen [5] Double Random Battles, Gen [1] Random Battles, Gen [2] Random Battles, Challenge Cup 1v1, Hackmons Cup, Battle Factory, Seasonal, Monotype Random Battle, Uber, OU, UU, RU, NU, PU, LC, AG, Doubles OU, No Status, MonsJustMons, Tier Shift, Monotype');
+	},
+
+	rpoll: 'randompoll',
+	randomspoll: 'randompoll',
+	randompoll: function (target, room, user) {
+		if (!this.can('broadcast', null, room)) return false;
+		this.parse('/poll Tier for the next tournament?, Random Battles, Double Random Battles, Triple Random Battles, Gen [5] Random Battles, Gen [5] Double Random Battles, Gen [1] Random Battles, Gen [2] Random Battles, Challenge Cup 1v1, Hackmons Cup, Double Hackmons Cup, Triple Hackmons Cup, Battle Factory, Seasonal, Monotype Random Battle');
+	},
+
+	fpoll: 'formatpoll',
+	formatspoll: 'formatpoll',
+	formatpoll: function (target, room, user) {
+		if (!this.can('broadcast', null, room)) return false;
 		this.parse('/poll Tier for the next tournament?,' + Object.values(Tools.data.Formats).filter(function (f) { return f.effectType === 'Format' && f.tournamentShow; }).map('name').join(", "));
 	},
 
