b52b9f4a559eb64c5e73bc7531a08228802e13cb
TheFenderStory <jared@thekards.com>
2015-08-12 09:32:44 -0700

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -155,7 +155,7 @@ exports.tellsexpiryage = 1000 * 60 * 60 * 24 * 7;
 // to send offline messages.
 exports.tellrank = '+';
 
-// poofOf - enable or disable the use of poofs in the lobby.
+// poofOff - enable or disable the use of poofs in the lobby.
 exports.poofOff = 'false';
 
 // database use to store user's money, tickets, etc.
