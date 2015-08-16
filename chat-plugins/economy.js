de0102d9ad48d679c54be3312301b6a3304cfb64
Phil <birkal@outlook.com>
2015-08-16 13:39:27 -0700

    Fix lint errors

--- chat-plugins/economy.js
+++ chat-plugins/economy.js
@@ -240,7 +240,7 @@ exports.commands = {
 		amount = amount + currencyName(amount);
 		userTotal = userTotal + currencyName(userTotal);
 		targetTotal = targetTotal + currencyName(targetTotal);
-		_this.sendReply("You have successfully transferred " + amount + ". You now have " + userTotal + ".");
+		this.sendReply("You have successfully transferred " + amount + ". You now have " + userTotal + ".");
 		if (Users.get(username)) Users.get(username).popup(user.name + " has transferred " + amount + ". You now have " + targetTotal + ".");
 		logMoney(user.name + " transferred " + amount + " to " + username + ". " + user.name + " now has " + userTotal + " and " + username + " now has " + targetTotal + ".");
 	},
