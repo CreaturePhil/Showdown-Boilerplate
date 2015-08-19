c3d8f22b7af20d20e2699632cec65ff3d8f15f91
Flareninja <flareninja98@gmail.com>
2015-08-19 02:21:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -374,8 +374,6 @@ if (Config.crashguard) {
 
 global.Sockets = require('./sockets.js');
 
-global.Bot = require('./bot.js');
-
 /*********************************************************
  * Set up our last global
  *********************************************************/
