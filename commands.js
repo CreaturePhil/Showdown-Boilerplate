74addd8ce408ce1d469de26ad547c502cab975c9
Flareninja <flareninja98@gmail.com>
2015-08-19 18:37:25 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -856,6 +856,30 @@ var commands = exports.commands = {
 		this.run('mute');
 	},
 	hourmutehelp: ["/hourmute OR /hm [username], [reason] - Mutes a user with reason for an hour. Requires: % @ # & ~"],
+	
+	dm: 'daymute',
+	daymute: function (target, room, user) {
+		if (!target) return this.parse('/help daymute');
+		if (!this.can('mute', targetUser, room)) return false;
+		if ((user.locked || user.mutedRooms[room.id]) && !user.can('bypassall')) return this.sendReply('You cannot do this while unable to talk.');
+
+		target = this.splitTarget(target);
+		var targetUser = this.targetUser;
+		if (!targetUser) return this.sendReply('User "' + this.targetUsername + '" not found.');
+
+		if (((targetUser.mutedRooms[room.id] && (targetUser.muteDuration[room.id] || 0) >= 50 * 60 * 1000) || targetUser.locked) && !target) {
+			var problem = ' but was already ' + (!targetUser.connected ? 'offline' : targetUser.locked ? 'locked' : 'muted');
+			return this.privateModCommand('(' + targetUser.name + ' would be muted by ' + user.name + problem + '.)');
+		}
+
+		targetUser.popup(user.name + ' has muted you for 24 hours. ' + target);
+		this.addModCommand('' + targetUser.name + ' was muted by ' + user.name + ' for 24 hours.' + (target ? " (" + target + ")" : ""));
+		var alts = targetUser.getAlts();
+		if (alts.length) this.addModCommand("" + targetUser.name + "'s alts were also muted: " + alts.join(", "));
+
+		targetUser.mute(room.id, 24 * 60 * 60 * 1000, true);
+	},
+	daymutehelp: ["/daymute OR /dm [username], [reason] - Mutes a user with reason for a day. Requires: % @ # & ~"],
 
 	um: 'unmute',
 	unmute: function (target, room, user) {
