2357b9b01d82506dc1301a846357fc02fa847be3
Flareninja <flareninja98@gmail.com>
2015-08-19 17:42:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -396,6 +396,7 @@ fs.readFile(path.resolve(__dirname, 'config/ipbans.txt'), function (err, data) {
 	}
 	Users.checkRangeBanned = Cidr.checker(rangebans);
 });
+global.Core = require('./custom/Core.js').Core;
 
 /*********************************************************
  * Start up the REPL server
