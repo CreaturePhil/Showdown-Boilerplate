6ca267cc68a08b1528f3c810fea21a8991ef816d
Jim <RubyD@users.noreply.github.com>
2015-09-05 23:39:35 +0100

    Add infobox-limited
    /emotes can become to big. This is to make it look somewhat better.

--- chat-plugins/emoticons.js
+++ chat-plugins/emoticons.js
@@ -124,7 +124,7 @@ function create_table() {
 		}
 	}
 
-	return "<center><b><u>List of Emoticons</u></b></center>" + "<table border='1' cellspacing='0' cellpadding='5' width='100%'>" + "<tbody>" + emotes_group_list.join("") + "</tbody>" + "</table>";
+	return "<center><b><u>List of Emoticons</u></b></center>" + "<div class='infobox-limited'><table border='1' cellspacing='0' cellpadding='5' width='100%'>" + "<tbody>" + emotes_group_list.join("") + "</tbody>" + "</table></div>";
 }
 
 var emotes_table = create_table();
