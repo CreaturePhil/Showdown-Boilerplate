2b37a7dbff2e5d98fca303fd31545cf2c35fce2e
Flareninja <flareninja98@gmail.com>
2015-09-22 12:54:17 +0200

    Update emoticons.js

--- chat-plugins/emoticons.js
+++ chat-plugins/emoticons.js
@@ -183,4 +183,13 @@ exports.commands = {
 		}
 	},
 	toggleemoticonshelp: ["/toggleemoticons - Toggle emoticons on or off."]
+	
+	rande: 'randemote',
+	randemote: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		var rng = Math.floor(Math.random() * emotesKeys.length);
+		var randomEmote = emotesKeys[rng];
+		this.sendReplyBox("<img src='" + emotes[randomEmote] + "' title='" + randomEmote + "' height='50' width='50' />");
+	},
+	randemotehelp: ["/randemote - Get a random emote."]
 };
