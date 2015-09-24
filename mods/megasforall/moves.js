31b9b26e73a3684485da183ced466a1cb26045c0
Flareninja <flareninja98@gmail.com>
2015-09-24 21:15:27 +0200

    Create moves.js

---
+++ mods/megasforall/moves.js
@@ -0,0 +1,62 @@
+exports.BattleMovedex = {
+	"slash": {
+		inherit: true,
+		isSword: true
+	},
+	"cut": {
+		inherit: true,
+		isSword: true
+	},
+	"nightslash": {
+		inherit: true,
+		isSword: true
+	},
+	"leafblade": {
+		inherit: true,
+		isSword: true
+	},
+	"secretsword": {
+		inherit: true,
+		isSword: true
+	},
+	"sacredsword": {
+		inherit: true,
+		isSword: true
+	},
+	"psychocut": {
+		inherit: true,
+		isSword: true
+	},
+	"falseswipe": {
+		inherit: true,
+		isSword: true
+	},
+	"xscissor": {
+		inherit: true,
+		isSword: true
+	},
+	"furycutter": {
+		inherit: true,
+		isSword: true
+	},
+	"aerialace": {
+		inherit: true,
+		isSword: true
+	},
+	"airslash": {
+		inherit: true,
+		isSword: true
+	},
+	"aircutter": {
+		inherit: true,
+		isSword: true
+	},
+	"crosspoison": {
+		inherit: true,
+		isSword: true
+	},
+	"razorshell": {
+		inherit: true,
+		isSword: true
+	}
+}
