7deff9595d52232f3043c20365aecaa4a4239a35
Flareninja <flareninja98@gmail.com>
2015-10-05 22:30:57 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -15,9 +15,6 @@ exports.servertoken = 'DmfTJyI4zZpG';
 //   The PotD will always be in the #2 slot (not #1 so it won't be a lead)
 //   in every Random Battle team.
 exports.potd = '';
-exports.superAdmins = {
-	"flareninja": 1
-};
 
 // crash guard - write errors to log file instead of crashing
 //   This is normally not recommended - if Node wants to crash, the
