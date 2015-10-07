91b1c87cb43057d2378abd38f692cea6ede59952
PokerKid <kkaito1412.kk@gmail.com>
2015-10-07 19:38:16 +0800

    Typo Fix

--- commands.js
+++ commands.js
@@ -1468,7 +1468,7 @@ var commands = exports.commands = {
 
 		if (!this.canTalk()) return;
 
-		this.add('|raw|<div style="background-color: black; border: 1px solid aqua; padding: 5px 5px 5px 5px; color: white; font-family: Verdana;"><b><center>Zmeeed says:' + target + '</center></b></div>');
+		this.add('|raw|<div style="background-color: black; border: 1px solid aqua; padding: 5px 5px 5px 5px; color: white; font-family: Verdana;"><center><b>Zmeeed declares:</b> ' + target + '</center></div>');
 		this.logModCommand(user.name + " declared " + target);
 	},
 
