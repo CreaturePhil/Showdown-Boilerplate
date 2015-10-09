391ce212a5ee5fe148d869e6463a9b0bc3b031bc
Flareninja <flareninja98@gmail.com>
2015-10-09 21:44:39 +0200

    Update ez-tc.js

--- chat-plugins/ez-tc.js
+++ chat-plugins/ez-tc.js
@@ -31,22 +31,27 @@ exports.commands = {
 
 		switch (parts[0]) {
 			case 'add':
-				if (!this.can('trainercard')) return false;
+				if (!this.can('pban')) return false;
 				if (!parts[2]) return this.sendReply("Usage: /trainercard add, [command name], [html]");
 				var commandName = toId(parts[1]);
 				if (CommandParser.commands[commandName]) return this.sendReply("/trainercards - The command \"" + commandName + "\" already exists.");
-				var html = parts.splice(2, parts.length).join(',');
-				trainerCards[commandName] = new Function('target', 'room', 'user', "if (!room.disableTrainerCards) if (!this.canBroadcast()) return; this.sendReplyBox('" + html.replace(/'/g, "\\'") + "');");
-				saveTrainerCards();
-				this.sendReply("The trainer card \"" + commandName + "\" has been added.");
-				this.logModCommand(user.name + " added the trainer card " + commandName);
+				try {
+					var html = parts.splice(2, parts.length).join(',');
+					trainerCards[commandName] = new Function('target', 'room', 'user', "if (!room.disableTrainerCards) if (!this.canBroadcast()) return; this.sendReplyBox('" + html.replace(/'/g, "\\'") + "');");
+					saveTrainerCards();
+					this.sendReply("The trainer card \"" + commandName + "\" has been added.");
+					this.logModCommand(user.name + " added the trainer card " + commandName);
+					Rooms.get('staff').add(user.name + " added the trainer card " + commandName);
+				} catch (e) {
+					this.errorReply("Something went wrong when trying to add this command.  Did you use a backwards slash mark?  If so, try it again without using this.");
+				}
 				break;
 
 			case 'rem':
 			case 'del':
 			case 'delete':
 			case 'remove':
-				if (!this.can('trainercard')) return false;
+				if (!this.can('pban')) return false;
 				if (!parts[1]) return this.sendReply("Usage: /trainercard remove, [command name]");
 				var commandName = toId(parts[1]);
 				if (!trainerCards[commandName]) return this.sendReply("/trainercards - The command \"" + commandName + "\" does not exist, or was added manually.");
@@ -55,6 +60,9 @@ exports.commands = {
 				saveTrainerCards();
 				this.sendReply("The trainer card \"" + commandName + "\" has been removed.");
 				this.logModCommand(user.name + " removed the trainer card " + commandName);
+				try {
+					Rooms.rooms.staff.add(user.name + " removed the trainer card " + commandName);
+				} catch (e) {};
 				break;
 
 			case 'list':
@@ -84,6 +92,19 @@ exports.commands = {
 				this.privateModCommand("(" + user.name + " has enabled broadcasting trainer cards in this room.)");
 				break;
 
+			case 'reload':
+				if (!this.can('pban')) return false;
+				return this.sendReply("Trainer cards have been reloaded.");
+				loadTrainerCards();
+				break;
+
+			case 'source':
+				if (!this.can('pban')) return false;
+				if (!parts[1]) return this.errorReply("Usage: /tc source, [commands] - displays the source code of a trainer card.");
+				if (!CommandParser.commands[parts[1]]) return this.errorReply("Command not found.  Check spelling?");
+				return this.sendReply(CommandParser.commands[parts[1]]);
+				break;
+
 			case 'info':
 			case 'help':
 			default:
@@ -95,6 +116,8 @@ exports.commands = {
 					"/trainercard list - Shows a list of all trainer cards added with this command.<br />" +
 					"/trainercard off - Disables broadcasting trainer cards in the current room.<br />" +
 					"/trainercard on - Enables broadcasting trainer cards in the current room.<br />" +
+					"/trainercard reload - Reloads trainer cards if they break (shouldn't happen).<br />" +
+					"/trainercard source, [command] - Displays the source code for a trainer card.<br />" +
 					"/trainercard help - Shows this help command.<br />" +
 					"<a href=\"https://gist.github.com/jd4564/399934fce2e9a5ae29ad\">EZ-TC Plugin by jd</a>"
 				);
