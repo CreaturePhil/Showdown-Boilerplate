fee296e3fd15c3f45903446d570f87dc66c96e09
Flareninja <flareninja98@gmail.com>
2015-10-10 14:23:53 +0200

    Update league.js

--- chat-plugins/league.js
+++ chat-plugins/league.js
@@ -102,7 +102,7 @@ exports.commands = {
 			userT = targetUser;
 		}
 		var medalId = League.findMedal(userT);
-		if (medalId) return this.sendReply(userT + " no es miembro de ninguna liga del servidor.");
+		if (medalId) return this.sendReply(userT + " is not member of any league.");
 		var medalData = League.getMedalData(medalId);
 		if (!medalData) return this.sendReply("The badge doesn't exist.");
 		return this.sendReplyBox(userT + ' puede hacer entrega de: <b>' + Tools.escapeHTML(medalData.name) + ':</b><br /><br /><img src="' + encodeURI(medalData.image) + '" title="' + Tools.escapeHTML(medalData.name) + '" width="' + Tools.escapeHTML(medalData.width) + '" height="' + Tools.escapeHTML(medalData.height) + '" />&nbsp;');
@@ -122,7 +122,7 @@ exports.commands = {
 	liga: function (target, room, user) {
 		if (!this.canBroadcast()) return false;
 		var leagueId = League.findLeague(target, room.id);
-		if (!leagueId) return this.sendReply("La liga especificada no está registrada en el servidor.");
+		if (!leagueId) return this.sendReply("The specified league is not registered on the server.");
 		return this.sendReplyBox(League.getLeagueTable(leagueId));
 	},
 	
@@ -134,24 +134,24 @@ exports.commands = {
 	newleague: 'addleague',
 	addleague: function (target, room, user) {
 		if (!this.can("league")) return false;
-		if (!target) return this.sendReply("No has especificado ninguna liga.");
+		if (!target) return this.sendReply("League is not specified.");
 		var params = target.split(',');
 		if (!params || params.length < 3) return this.sendReply("Usage: /addleague [id], [name], [room]");
-		if (League.newLeague(params[0], params[1], params[2])) return this.sendReply("Liga: " + toId(params[0]) + " creada con exito.");
+		if (League.newLeague(params[0], params[1], params[2])) return this.sendReply("League: " + toId(params[0]) + " successful created.");
 		this.sendReply("This league already exist.");
 	},
 	
 	deleteleague: function (target, room, user) {
 		if (!this.can("league")) return false;
-		if (!target) return this.sendReply("No has especificado ninguna liga.");
-		if (League.deleteLeague(target)) return this.sendReply("Liga: " + toId(target) + " eliminada con exito.");
+		if (!target) return this.sendReply("League is not specified.");
+		if (League.deleteLeague(target)) return this.sendReply("League: " + toId(target) + " successful eliminated.");
 		this.sendReply("The league doesn't exist.");
 	},
 	
 	eleague: 'editleague',
 	editleague: function (target, room, user) {
 		if (!this.can("league")) return false;
-		if (!target) return this.sendReply("No has especificado ninguna liga.");
+		if (!target) return this.sendReply("League is not specified.");
 		var params = target.split(',');
 		if (!params || params.length < 3) return this.sendReply("Usage: /editleague [id], [name/room], [data]");
 		var opc = toId(params[1]).substr(0,1);
@@ -165,7 +165,7 @@ exports.commands = {
 		var params = target.split(',');
 		if (!params || params.length < 3) return this.sendReply("Usage: /setgymleader [id-league], [user], [id-medal]");
 		if (!Users.get(params[1])) this.sendReply('Warning: ' + toId(params[1]) + ' is offline.');
-		if (League.addLeader(params[0], params[1], 'g', params[2])) return this.sendReply('Usuario ' + toId(params[1]) + ' asignado en el puesto correspondiente.');
+		if (League.addLeader(params[0], params[1], 'g', params[2])) return this.sendReply('User ' + toId(params[1]) + ' assigned to gym leader.');
 		this.sendReply("Some of this dates is incorrect.");
 	},
 	
@@ -175,7 +175,7 @@ exports.commands = {
 		var params = target.split(',');
 		if (!params || params.length < 3) return this.sendReply("Usage: /setelite [id-league], [user], [id-medal]");
 		if (!Users.get(params[1])) this.sendReply('Warning: ' + toId(params[1]) + ' is offline.');
-		if (League.addLeader(params[0], params[1], 'e', params[2])) return this.sendReply('Usuario ' + toId(params[1]) + ' asignado en el puesto correspondiente.');
+		if (League.addLeader(params[0], params[1], 'e', params[2])) return this.sendReply('User ' + toId(params[1]) + ' assigned to elite.');
 		this.sendReply("Some of this dates is incorrect.");
 	},
 	
@@ -184,7 +184,7 @@ exports.commands = {
 		if (!target) return this.sendReply('Usage: /removegymleader [id-league], [id-medal]');
 		var params = target.split(',');
 		if (!params || params.length < 2) return this.sendReply("Usage: /removegymleader [id-league], [id-medal]");
-		if (League.removeLeader(params[0], params[1])) return this.sendReply('Puesto de la liga especificada borrado con exito.');
+		if (League.removeLeader(params[0], params[1])) return this.sendReply('Role removed successfully.');
 		this.sendReply("Some of this dates is incorrect.");
 	},
 
@@ -195,22 +195,22 @@ exports.commands = {
 		var params = target.split(',');
 		if (params.length === 1) {
 			var userT = Users.get(params[0]);
-			if (!userT) return this.sendReply('El usuario ' + toId(target) + ' no existe o no está disponible.');
+			if (!userT) return this.sendReply('User ' + toId(target) + ' does not exist or is not available.');
 			var league = League.findLeagueFromRoom(room.id);
-			if (!league) return this.sendReply('Este comando solo puede ser usado en la Sala correspondiente a la liga.');
+			if (!league) return this.sendReply('This command can only be used in a league.');
 			var medalId = League.findMedal(user.name, league);
-			if (!medalId) return this.sendReply('No estas registrado como miembro de la liga ' + league);
+			if (!medalId) return this.sendReply('Not registered as a member of the league ' + league);
 			var medalData = League.getMedalData(medalId);
-			if (!League.giveMedal(medalId, params[0])) return this.sendReply('El usuario ya poseía la medalla que intentas entregar.');
+			if (!League.giveMedal(medalId, params[0])) return this.sendReply('The user already has this badge');
 			userT.popup(user.name + " te ha entregado la siguiente medalla: " + medalData.name + "\nRecuerda que puedes comproar tus medallas con el comando /medallas");
 			this.addModCommand(user.name + " ha entregado su medalla (" + medalData.name + ") a " + toId(target) + '.');
 			return;
 		} else if(params.length > 1){
 			if (!this.can('league')) return false;
 			var userT = Users.get(params[0]);
-			if (!userT) return this.sendReply('El usuario ' + toId(params[0]) + ' no existe o no está disponible.');
-			if (!League.giveMedal(params[1], params[0])) return this.sendReply('El usuario ya poseía dicha medalla o el Id es incorrecto.');
-			return this.sendReply('Medalla (' + League.getMedalData(params[1]).name + ') entregada a ' + toId(params[0]) + '.');
+			if (!userT) return this.sendReply('El usuario ' + toId(params[0]) + ' does not exist or is not available.');
+			if (!League.giveMedal(params[1], params[0])) return this.sendReply('The user already have that medal or ID is incorrect');
+			return this.sendReply('Badge (' + League.getMedalData(params[1]).name + ') gived to ' + toId(params[0]) + '.');
 		}
 		return this.sendReply('Usage: /darmedalla [user], (id)');
 	},
@@ -223,13 +223,13 @@ exports.commands = {
 			var league = League.findLeagueFromRoom(room.id);
 			if (!league) return this.sendReply('This command can only be used in a league');
 			var medalId = League.findMedal(user.name, league);
-			if (!medalId) return this.sendReply('No estas registrado como miembro de la liga ' + league);
-			if (!League.removeMedal(medalId, params[0])) return this.sendReply('El usuario ya poseía la medalla que intentas entregar.');
-			this.addModCommand(user.name + " ha retirado su medalla a " + toId(target) + '.');
+			if (!medalId) return this.sendReply('Not registered as a member of the league ' + league);
+			if (!League.removeMedal(medalId, params[0])) return this.sendReply('The user already has this badge.');
+			this.addModCommand(user.name + " has token is badge from " + toId(target) + '.');
 			return;
 		} else if(params.length > 1){
 			if (!this.can('league')) return false;
-			if (!League.removeMedal(params[1], params[0])) return this.sendReply('El usuario no poseía dicha medalla o el Id es incorrecto.');
+			if (!League.removeMedal(params[1], params[0])) return this.sendReply('The user does not have that medal or ID is incorrect.');
 			return this.sendReply('Badge (' + League.getMedalData(params[1]).name + ') taken from ' + toId(params[0]) + '.');
 		}
 		return this.sendReply('Usage: /quitarmedalla [user], (id)');
