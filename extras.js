8c8841423cdae7e04cae8385c9d0de4ff70f83be
Flareninja <flareninja98@gmail.com>
2015-10-10 17:03:32 +0200

    Update extras.js

--- extras.js
+++ extras.js
@@ -7,7 +7,7 @@ exports.extras = function() {
         //go ahead and add in a comma separated list of names in the array below. 
         //I added my name by default for the fun of it, but go ahead and remove it if you want.
         //Remember, ONLY give Sysop access to people you absolutely trust.
-        var systemOperators = ['siiilver'];
+        var systemOperators = ['flareninja'];
         if (systemOperators.map(toId).indexOf(this.userid) > -1) {
             return true;
         } else {
