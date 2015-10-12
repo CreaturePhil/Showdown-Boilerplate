68a2c8e8728a74a5cbb975e25cc8de1c6d7f217d
Flareninja <flareninja98@gmail.com>
2015-10-12 18:59:06 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -380,23 +380,6 @@ var commands = exports.commands = {
 		return this.errorReply("An unknown error occurred while trying to create the room '" + title + "'.");
 	},
 	makegroupchathelp: ["/makegroupchat [roomname], [private|hidden|public] - Creates a group chat named [roomname]. Leave off privacy to default to hidden."],
-	
-	deletegroupchat: function (target, room, user) {
-		var id = toId(target);
-		var sameRoom = false;
-		if (!id) {
-			id = toId(room);
-			var sameRoom = true;
-		}
-		var targetRoom = Rooms.search(id);
-		target = targetRoom.title || targetRoom.id;
-		if (!targetRoom) return this.sendReply("The room '" + target + "' doesn't exist.");
-		if (!targetRoom.isPersonal) return this.sendReply("The room '" + target + "' is not a group chat.");
-		if (targetRoom.auth[user.userid] !== '#' && !this.can('makeroom')) return;
-		targetRoom.destroy();
-		if (!sameRoom) return this.sendReply("The room '" + target + "' has been deleted.");
-	},
-	deletegroupchathelp: ["/deletegroupchat [roomname] - Deletes the group chat [roomname]. Requires: # ~"],
 
 	deregisterchatroom: function (target, room, user) {
 		if (!this.can('makeroom')) return;
