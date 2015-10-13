b31c9a874c6dd0d6ccf7c06dfb89924c4fa83837
Flareninja <flareninja98@gmail.com>
2015-10-13 16:02:09 +0200

    Update trainer-cards.js

--- chat-plugins/trainer-cards.js
+++ chat-plugins/trainer-cards.js
@@ -1,21 +1,74 @@
 exports.commands = {
-  dueloffates: function (target, room, user) {
+        dueloffates: function (target, room, user) {
 		if (!this.canBroadcast()) return;
-		this.sendReplyBox('<center><font size=1 color=#0000><b>Duel Of Fates</b> - <i>The Phantom Menace</i></font></center>' +
+		this.sendReplyBox('<center><font size=2 color=#0000><b>Duel Of Fates</b> - <i>The Phantom Menace</i></font></center>' +
 		'<audio src="http://puu.sh/kdpjM/889962c97d.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
-	},
-	battleofheroes: function (target, room, user) {
-		if (!this.canBroadcast()) return;
-		this.sendReplyBox('<audio src="" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
 	},
 	gerudovalleyremix: function (target, room, user) {
 		if (!this.canBroadcast()) return;
-		this.sendReplyBox('<audio src="http://puu.sh/kHThZ/9caabdc1b9.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
+		this.sendReplyBox('<center><font size=2 color=#0000><b>Gerudo Valley Remix SSB4</b> - <i>Ocarina Of Time</i></font></center>' +
+		'<audio src="http://puu.sh/kHThZ/9caabdc1b9.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
 	},
 	freesong: function (target, room, user) {
 		if (!this.canBroadcast()) return;
-		this.sendReplyBox('<audio src="http://puu.sh/kHI3w/c1c04a46b0.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
-	}
+		this.sendReplyBox('<center><font size=2 color=#0000><b>Freesong</b> - <i>Free</i></font></center>' +
+		'<audio src="http://puu.sh/kHI3w/c1c04a46b0.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
+	},
+	battleofheroes: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox('<center><font size=2 color=#0000><b>Battle Of Heroes</b> - <i>Revenge Of The Sith</i></font></center>' +
+		'<audio src="http://puu.sh/kIHDH/40765467bd.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
+	},
+	imperialmarch: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox('<center><font size=2 color=#0000><b>Imperial March</b> - <i>The Empire Strikes Back</i></font></center>' +
+		'<audio src="http://puu.sh/kIHIn/61d7337df2.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
+	},
+	endorvictory: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox('<center><font size=2 color=#0000><b>Endor Victory</b> - <i>Return Of The Jedi</i></font></center>' +
+		'<audio src="http://puu.sh/kIHQe/b4c8a3f71b.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
+	},
+	balladofthegoddess: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox('<center><font size=2 color=#0000><b>Ballad Of The Goddess</b> - <i>Skyward Sword</i></font></center>' +
+		'<audio src="http://puu.sh/kIIiL/b9f081a2cf.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
+	},
+	dragonroostisland: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox('<center><font size=2 color=#0000><b>Dragon Roost Island</b> - <i>Wind Waker</i></font></center>' +
+		'<audio src="http://puu.sh/kIIoM/8887fd7bca.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
+	},
+	driftveilcity: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox('<center><font size=2 color=#0000><b>Driftveil City</b> - <i>Black/White</i></font></center>' +
+		'<audio src="http://puu.sh/kIJTz/e0266d338b.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
+	},
+	routeten: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox('<center><font size=2 color=#0000><b>Route 10 SSB4</b> - <i>Black/White</i></font></center>' +
+		'<audio src="http://puu.sh/kIKdn/255da9ec1c.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
+	},
+	starwars: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox('<center><font size=2 color=#0000><b>Star Wars Theme</b> - <i>Star Wars</i></font></center>' +
+		'<audio src="http://puu.sh/kIKU5/1e550a40d1.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
+	},
+	cynthia: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox('<center><font size=2 color=#0000><b>vs Cynthia SSB4</b> - <i>Diamond/Pearl/Platinum</i></font></center>' +
+		'<audio src="http://puu.sh/kIL1W/bef60bbc11.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
+	},
+	zinnia: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox('<center><font size=2 color=#0000><b>vs Zinnia</b> - <i>Omega Ruby/Alpha Sapphire</i></font></center>' +
+		'<audio src="http://puu.sh/kILOq/17caaa26a0.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
+	},
+	magmaaqua: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox('<center><font size=2 color=#0000><b>vs Magma/Aqua Leader</b> - <i>Omega Ruby/Alpha Sapphire</i></font></center>' +
+		'<audio src="http://puu.sh/kILVA/f92db22d33.mp3" controls="" style="width: 100% ; border: 1px solid #3B3B3B ; background-color: #00000a" target="_blank" />');
+	},
 };
 	
 
