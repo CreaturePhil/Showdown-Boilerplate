a575b844fda35848d646a75e7a0a57fcf3802832
Flareninja <flareninja98@gmail.com>
2015-10-16 22:41:27 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -180,7 +180,12 @@ exports.mysql = {
 //
 // Your server *must* be registered in order for your custom avatars to be
 // displayed in the client.
-exports.customavatars = JSON.parse(require('fs').readFileSync('config/customavatars.json'));
+exports.customavatars = {
+	//'userid': 'customavatar.png'
+};
+
+// custom avatars appear in profile by specifiying server url.
+exports.avatarurl = '';
 
 // Tournament announcements
 // When tournaments are created in rooms listed below, they will be announced in
