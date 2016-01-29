9d715f031a48a0140cf0e2e5f95d20fa3304bf74
Phil <birkal@outlook.com>
2016-01-28 16:46:31 -0800

    Update customcolour

--- config/color.js
+++ config/color.js
@@ -127,13 +127,38 @@ function toHex(e) {
 
 var colorCache = {};
 
-function hashColor(e) {
-	if (colorCache[e]) return colorCache[e];
-	var t = MD5(e);
-	var n = parseInt(t.substr(4, 4), 16) % 360;
-	var r = parseInt(t.substr(0, 4), 16) % 50 + 50;
-	var i = parseInt(t.substr(8, 4), 16) % 20 + 25;
-	var s = hslToRgb(n, r, i);
-	colorCache[e] = "#" + rgbToHex(s.r, s.g, s.b);
-	return colorCache[e];
+function hashColor(name) {
+	name = toId(name);
+	if (colorCache[name]) return colorCache[name];
+	var hash = MD5(name);
+	var H = parseInt(hash.substr(4, 4), 16) % 360; // 0 to 360
+	var S = parseInt(hash.substr(0, 4), 16) % 50 + 40; // 40 to 89
+	var L = Math.floor(parseInt(hash.substr(8, 4), 16) % 20 + 30); // 30 to 49
+	var C = (100 - Math.abs(2 * L - 100)) * S / 100 / 100;
+	var X = C * (1 - Math.abs((H / 60) % 2 - 1));
+	var m = L / 100 - C / 2;
+
+	var R1, G1, B1;
+	switch (Math.floor(H / 60)) {
+		case 1: R1 = X; G1 = C; B1 = 0; break;
+		case 2: R1 = 0; G1 = C; B1 = X; break;
+		case 3: R1 = 0; G1 = X; B1 = C; break;
+		case 4: R1 = X; G1 = 0; B1 = C; break;
+		case 5: R1 = C; G1 = 0; B1 = X; break;
+		case 0: default: R1 = C; G1 = X; B1 = 0; break;
+	}
+	var lum = (R1 + m) * 0.2126 + (G1 + m) * 0.7152 + (B1 + m) * 0.0722; // 0.05 (dark blue) to 0.93 (yellow)
+	var HLmod = (lum - 0.5) * -100; // -43 (yellow) to 45 (dark blue)
+	if (HLmod > 12) HLmod -= 12;
+	else if (HLmod < -10) HLmod = (HLmod + 10) * 2 / 3;
+	else HLmod = 0;
+
+	L += HLmod;
+	var Smod = 10 - Math.abs(50 - L);
+	if (HLmod > 15) Smod += (HLmod - 15) / 2;
+	S -= Smod;
+
+	var rgb = hslToRgb(H, S, L);
+	colorCache[name] = "#" + rgbToHex(rgb.r, rgb.g, rgb.b);
+	return colorCache[name];
 }
