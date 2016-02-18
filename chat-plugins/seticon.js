657d209d75a308fa63d797516d62ca01e2030fcd
Kevinxzllz <kevinfalcon1509@gmail.com>
2016-02-17 20:41:26 -0430

    Create seticon.js

---
+++ chat-plugins/seticon.js
@@ -0,0 +1,30 @@
+/* 
+* Created by: Master Float.
+*/
+
+var fs = require('fs');
+var selectors;
+
+function writeIconCSS() {
+        fs.appendFile('config/custom.css', selectors);
+}
+ 
+exports.commands = {
+        seticon: function (target, room, user) {
+        if (!this.can('eval')) return this.errorReply("Access denied.");
+ 
+                var args = target.split(',');
+                if (args.length < 3) return this.parse('/help seticon');
+                var username = toId(args.shift());
+                var image = 'background: rgba(244, 244, 244, 0.8) url("' + args.shift().trim() + '") right no-repeat;';
+                selectors = '\n\n' + '  #' + toId(args.shift()) + '-userlist-user-' + username;
+                args.forEach(function (room) {
+                        selectors += ', #' + toId(room) + '-userlist-user-' + username;
+                });
+                selectors += ' { \n' + '    ' + image +  '\n  }';
+ 
+                this.privateModCommand("(" + user.name + " has set an icon to  " + username + ")");
+                writeIconCSS();
+        },
+        seticonhelp: ["/seticon [username], [image], [room 1], [room 2], etc. - Sets an icon to a user in chosen rooms."]
+};
