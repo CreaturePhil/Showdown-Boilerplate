524fe3a8452c3d18400fe52d5a4c7418f3023ac3
thearkguy <anubhavkumarc@googlemail.com>
2016-05-28 07:51:39 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -20,8 +20,8 @@ exports.potd = '';
 // crash guard - write errors to log file instead of crashing
 //   This is normally not recommended - if Node wants to crash, the
 //   server needs to be restarted
-//   Unfortunately, socket.io bug 409 requires some sort of crash guard
-//   https://github.com/LearnBoost/socket.io/issues/609
+//   However, most people want the server to stay online even if there is a
+//   crash, so this option is provided
 exports.crashguard = true;
 
 // login server data - don't forget the http:// and the trailing slash
@@ -63,6 +63,13 @@ exports.loginserverpublickey = "-----BEGIN RSA PUBLIC KEY-----\n" +
 	subject: 'Pokemon Showdown has crashed!'
 };**/
 
+// basic name filter - removes characters used for impersonation
+//   The basic name filter removes Unicode characters that can be used for impersonation,
+//   like the upside-down exclamation mark (looks like an i), the Greek omicron (looks
+//   like an o), etc. Disable only if you need one of the alphabets it disables, such as
+//   Greek or Cyrillic.
+exports.disablebasicnamefilter = false;
+
 // report joins and leaves - shows messages like "<USERNAME> joined"
 //   Join and leave messages are small and consolidated, so there will never
 //   be more than one line of messages.
@@ -99,9 +106,9 @@ exports.reportbattlejoins = true;
 // chat modchat - default minimum group for speaking in chatrooms; changeable with /modchat
 exports.chatmodchat = false;
 // battle modchat - default minimum group for speaking in battles; changeable with /modchat
-exports.battlemodchat = false;
+exports.battlemodchat = true;
 // pm modchat - minimum group for PMing other users, challenging other users, and laddering
-exports.pmmodchat = false;
+exports.pmmodchat = true;
 
 // forced timer - force the timer on for all battles
 //   Players will be unable to turn it off.
@@ -186,7 +193,10 @@ exports.avatarurl = '';
 // When tournaments are created in rooms listed below, they will be announced in
 // the server's main tournament room (either the specified tourroom or by default
 // the room 'tournaments')
-exports.tourroom = '';
+// tourroom - specify a room to receive tournament announcements (defaults to
+// the room 'tournaments').
+// tourannouncements - announcements are only allowed in these rooms
+exports.tourroom = 'dragonrusharena';
 exports.tourannouncements = [/* roomids */];
 
 // appealurl - specify a URL containing information on how users can appeal
@@ -248,6 +258,9 @@ exports.replsocketmode = 0o600;
 //     - tournaments: creating tournaments (/tour new, settype etc.)
 //     - tournamentsmoderation: /tour dq, autodq, end etc.
 //     - tournamentsmanagement: enable/disable tournaments.
+//     - minigame: make minigames (hangman, polls, etc.).
+//     - game: make games.
+//     - gamemanagement: enable/disable games and minigames.
 exports.grouplist = [
 	{   symbol: '♚',
 	    id: "dragonking",
@@ -296,6 +309,7 @@ exports.grouplist = [
 		disableladder: true,
 		globalonly: true,
 		tournamentsmanagement: true,
+		gamemanagement: true,
 	},
 	{
 		symbol: '#',
@@ -310,6 +324,7 @@ exports.grouplist = [
 		modchatall: true,
 		roomonly: true,
 		tournamentsmanagement: true,
+		gamemanagement: true,
 	},
 	{
 		symbol: '\u2605',
@@ -335,6 +350,7 @@ exports.grouplist = [
 		ip: true,
 		alts: '@u',
 		tournaments: true,
+		game: true,
 	},
 	{
 		symbol: '%',
@@ -356,6 +372,7 @@ exports.grouplist = [
 		tournamentsmoderation: true,
 		jeopardy: true,
 		joinbattle: true,
+		minigame: true,
 	},
 	{
 		symbol: '$',
