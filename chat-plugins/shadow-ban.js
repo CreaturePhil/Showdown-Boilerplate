5e3a43b5eeb05c0f8063eb0b573d832d3722f5b6
Lord-Haji <lordhajisinbox@gmail.com>
2016-06-02 10:38:18 +0530

    Update shadow-ban.js

--- chat-plugins/shadow-ban.js
+++ chat-plugins/shadow-ban.js
@@ -1,5 +1,7 @@
+'use strict';
+
 const ROOM_NAME = "Shadow Ban Room";
-var room = Rooms.get(toId(ROOM_NAME));
+let room = Rooms.get(toId(ROOM_NAME));
 if (!room) {
 	Rooms.global.addChatRoom(ROOM_NAME);
 	room = Rooms.get(toId(ROOM_NAME));
@@ -27,12 +29,12 @@ if (Object.size(room.addedUsers) > 0) {
 exports.room = room;
 
 function getAllAlts(user) {
-	var targets = {};
+	let targets = {};
 	if (typeof user === 'string') {
 		targets[toId(user)] = 1;
 	} else {
 		user.getAlts().concat(user.name).forEach(function (altName) {
-			var alt = Users.get(altName);
+			let alt = Users.get(altName);
 			if (!alt.named) return;
 
 			targets[toId(alt)] = 1;
@@ -45,14 +47,14 @@ function getAllAlts(user) {
 }
 
 function intersectAndExclude(a, b) {
-	var intersection = [];
-	var exclusionA = [];
-	var exclusionB = [];
+	let intersection = [];
+	let exclusionA = [];
+	let exclusionB = [];
 
-	var ai = 0;
-	var bi = 0;
+	let ai = 0;
+	let bi = 0;
 	while (ai < a.length && bi < b.length) {
-		var difference = a[ai].localeCompare(b[bi]);
+		let difference = a[ai].localeCompare(b[bi]);
 		if (difference < 0) {
 			exclusionA.push(a[ai]);
 			++ai;
@@ -71,18 +73,18 @@ function intersectAndExclude(a, b) {
 	return {intersection: intersection, exclusionA: exclusionA, exclusionB: exclusionB};
 }
 
-var checkBannedCache = {};
-var checkBanned = exports.checkBanned = function (user) {
-	var userId = toId(user);
+let checkBannedCache = {};
+let checkBanned = exports.checkBanned = function (user) {
+	let userId = toId(user);
 	if (userId in checkBannedCache) return checkBannedCache[userId];
 	//console.log("Shadow ban cache miss:", userId);
 
-	var targets = Object.keys(getAllAlts(user)).sort();
-	var bannedUsers = Object.keys(room.addedUsers).sort();
+	let targets = Object.keys(getAllAlts(user)).sort();
+	let bannedUsers = Object.keys(room.addedUsers).sort();
 
-	var matches = intersectAndExclude(targets, bannedUsers);
-	var isBanned = matches.intersection.length !== 0;
-	for (var t = 0; t < targets.length; ++t) {
+	let matches = intersectAndExclude(targets, bannedUsers);
+	let isBanned = matches.intersection.length !== 0;
+	for (let t = 0; t < targets.length; ++t) {
 		if (isBanned) room.addedUsers[targets[t]] = 1;
 		checkBannedCache[targets[t]] = isBanned;
 	}
@@ -96,9 +98,9 @@ var checkBanned = exports.checkBanned = function (user) {
 	return true;
 };
 
-var addUser = exports.addUser = function (user) {
-	var targets = getAllAlts(user);
-	for (var u in targets) {
+let addUser = exports.addUser = function (user) {
+	let targets = getAllAlts(user);
+	for (let u in targets) {
 		if (room.addedUsers[u]) {
 			delete targets[u];
 		} else {
@@ -116,9 +118,9 @@ var addUser = exports.addUser = function (user) {
 
 	return targets;
 };
-var removeUser = exports.removeUser = function (user) {
-	var targets = getAllAlts(user);
-	for (var u in targets) {
+let removeUser = exports.removeUser = function (user) {
+	let targets = getAllAlts(user);
+	for (let u in targets) {
 		if (!room.addedUsers[u]) {
 			delete targets[u];
 		} else {
@@ -137,7 +139,7 @@ var removeUser = exports.removeUser = function (user) {
 	return targets;
 };
 
-var addMessage = exports.addMessage = function (user, tag, message) {
+let addMessage = exports.addMessage = function (user, tag, message) {
 	room.add('|c|' + user.getIdentity() + '|__(' + tag + ')__ ' + message);
 	room.update();
 };
@@ -149,9 +151,9 @@ exports.commands = {
 		if (["~", "&"].indexOf(user.group) === -1) return this.errorReply("The command '/" + cmd + "' was unrecognized. To send a message starting with '/" + cmd + "', type '//" + cmd + "'.");
 		if (!target) return this.sendReply("/shadowban OR /sban [username], [secondary command], [reason] - Sends all the user's messages to the shadow ban room.");
 
-		var params = this.splitTarget(target).split(',');
-		var action = params[0].trim().toLowerCase();
-		var reason = params.slice(1).join(',').trim();
+		let params = this.splitTarget(target).split(',');
+		let action = params[0].trim().toLowerCase();
+		let reason = params.slice(1).join(',').trim();
 		if (!(action in CommandParser.commands)) {
 			action = 'mute';
 			reason = params.join(',').trim();
@@ -159,7 +161,7 @@ exports.commands = {
 
 		if (!this.targetUser) return this.sendReply("User '" + this.targetUsername + "' not found.");
 		
-		var targets = addUser(this.targetUser);
+		let targets = addUser(this.targetUser);
 		if (targets.length === 0) {
 			return this.sendReply('||' + this.targetUsername + " is already shadow banned or isn't named.");
 		}
@@ -177,7 +179,7 @@ exports.commands = {
 		this.splitTarget(target);
 
 
-		var targets = removeUser(this.targetUser || this.targetUsername);
+		let targets = removeUser(this.targetUser || this.targetUsername);
 		if (targets.length === 0) {
 			return this.sendReply('||' + this.targetUsername + " is not shadow banned.");
 		}
