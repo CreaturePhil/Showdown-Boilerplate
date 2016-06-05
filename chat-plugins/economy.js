ff6d0183374ffd975c1c86d1054edbee5fd3fa8d
BlazingAura <tallha.mulla@gmail.com>
2016-06-05 17:38:50 +0500

    Update economy.js

--- chat-plugins/economy.js
+++ chat-plugins/economy.js
@@ -383,7 +383,7 @@ exports.commands = {
 		const total = users.reduce(function (acc, cur) {
 			return acc + Db('money').get(cur);
 		}, 0);
-		let average = Math.floor(total / users.length);
+		let average = Math.floor(total / users.length) || '0';
 		let output = "There " + (total > 1 ? "are " : "is ") + total + currencyName(total) + " circulating in the economy. ";
 		output += "The average user has " + average + currencyName(average) + ".";
 		this.sendReplyBox(output);
