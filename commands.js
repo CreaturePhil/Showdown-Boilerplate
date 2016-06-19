9b3c92a352ad655407b82269cc4c0cfcf64daf9c
FlameOverload <flameoverload@gmail.com>
2016-06-19 16:24:39 +0530

    Add room founder

--- commands.js
+++ commands.js
@@ -744,6 +744,48 @@ exports.commands = {
 			Rooms.global.writeChatRoomData();
 		}
 	},
+		roomfounder: function (target, room, user) {
+		if (!room.chatRoomData) {
+			return this.errorReply("/roomfounder - This room isn\'t designed for per-room moderation to be added.");
+		}
+		target = this.splitTarget(target, true);
+		let targetUser = this.targetUser;
+		if (!targetUser) return this.errorReply("User '" + this.targetUsername + "' is not online.");
+		if (!this.can('makeroom')) return false;
+		if (!room.auth) room.auth = room.chatRoomData.auth = {};
+		room.auth[targetUser.userid] = '#';
+		room.founder = targetUser.userid;
+		this.addModCommand(targetUser.name + ' was appointed to Room Founder by ' + user.name + '.');
+		if (needsPopup) targetUser.popup('|modal||html|You were appointed to Room Founder by ' + nameColor(user.name, true) + ' in ' + room.title + '.');
+		room.onUpdateIdentity(targetUser);
+		room.chatRoomData.founder = room.founder;
+		Rooms.global.writeChatRoomData();
+	},
+
+	roomdefounder: 'deroomfounder',
+	deroomfounder: function (target, room, user) {
+		if (!room.auth) {
+			return this.errorReply("/roomdefounder - This room isn't designed for per-room moderation");
+		}
+		target = this.splitTarget(target, true);
+		let targetUser = this.targetUser;
+		let name = this.targetUsername;
+		let userid = toId(name);
+		if (!userid || userid === '') return this.errorReply("User '" + name + "' does not exist.");
+
+		if (room.auth[userid] !== '#') return this.errorReply("User '" + name + "' is not a room founder.");
+		if (!this.can('makeroom', null, room)) return false;
+
+		delete room.auth[userid];
+		delete room.founder;
+		this.addModCommand("(" + name + " is no longer Room Founder.)");
+		targetUser.send('(You are no longer Room Founder in ' + room.title + '.' + ')');
+                if (needsPopup) targetUser.popup('Your Room Founder status in ' + room.title + ' was taken by' + nameColor(user.name, true));
+		if (targetUser) targetUser.updateIdentity();
+		if (room.chatRoomData) {
+			Rooms.global.writeChatRoomData();
+		}
+	},
 
 	roomowner: function (target, room, user) {
 		if (!room.chatRoomData) {
