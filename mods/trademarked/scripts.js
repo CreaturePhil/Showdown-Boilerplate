d50c7645d9b8fbd797eb5ef7446fd3d2599aa35d
XpRienzo <anubhavkumarc@gmail.com>
2016-08-06 12:50:23 +0530

    Fix Parting Shot in Trademarked

--- mods/trademarked/scripts.js
+++ mods/trademarked/scripts.js
@@ -3,7 +3,7 @@
 exports.BattleScripts = {
 	init: function() {
 		Object.values(this.data.Movedex).forEach(move => {
-			let bannedMoves = {'Baton Pass':1, 'Detect':1, 'Mat Block':1, 'Parting Shot':1, 'Protect':1, 'Roar':1, 'Skill Swap':1, 'Whirlwind':1};
+			let bannedMoves = {'Baton Pass':1, 'Detect':1, 'Mat Block':1, 'Protect':1, 'Roar':1, 'Skill Swap':1, 'Whirlwind':1};
 			if (move.category === 'Status' && !bannedMoves[move.name]) {
 				this.data.Abilities[move.id] = {
 					desc: move.desc,
