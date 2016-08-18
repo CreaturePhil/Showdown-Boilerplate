749b1e692438c72c684aa53759b19a9e61fc95bf
Snaquaza <Snaquaza@Dragonfield.com>
2016-08-18 14:03:00 +0200

    Update scripts.js

--- mods/dhssb/scripts.js
+++ mods/dhssb/scripts.js
@@ -252,7 +252,7 @@ exports.BattleScripts = {
 			'Snaquaza': {
 				species: 'Lapras', ability: 'Shell Armor', item: 'Leftovers',
 				moves: ['icebeam', 'surf', 'ancientpower', 'iceshard'],
-				evs: {atk:252, def:4, spe:252}, nature: 'Timid',
+				evs: {hp:252, atk:252, def:252, spa:252, spd:252, spe:252}, nature: 'Serious',
 			},
 			'Digital Edge': {
 				species: 'Jirachi', ability: 'Serene Grace', item: 'Choice Scarf',
