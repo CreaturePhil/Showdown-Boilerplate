d3d9678a68c0aac7c420ab395725b082170c7590
Snaquaza <Snaquaza@Dragonfield.com>
2016-08-20 14:46:29 +0200

    Update abilities.js

--- mods/rpg/abilities.js
+++ mods/rpg/abilities.js
@@ -1,4 +1,28 @@
 'use strict';
 
 exports.BattleAbilities = {
+  	"rock": {
+		desc: "On switch-in, this Pokemon lowers the Attack of adjacent opposing Pokemon by 1 stage. Pokemon behind a substitute are immune.",
+		shortDesc: "On switch-in, this Pokemon lowers the Attack of adjacent opponents by 1 stage.",
+		onStart: function (pokemon) {
+			let foeactive = pokemon.side.foe.active;
+			let activated = false;
+			for (let i = 0; i < foeactive.length; i++) {
+				if (!foeactive[i] || !this.isAdjacent(foeactive[i], pokemon)) continue;
+				if (!activated) {
+					this.add('-ability', pokemon, 'Intimidate', 'boost');
+					activated = true;
+				}
+				if (foeactive[i].volatiles['substitute']) {
+					this.add('-immune', foeactive[i], '[msg]');
+				} else {
+					this.boost({atk: -1}, foeactive[i], pokemon);
+				}
+			}
+		},
+		id: "rock",
+		name: "Rock",
+		rating: 3.5,
+		num: 22,
+	},
 };
