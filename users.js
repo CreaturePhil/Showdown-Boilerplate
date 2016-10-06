1bc6ce1418133a4def3f4424078e11daca60cb02
Spandan <spandan.punwatkar@gmail.com>
2016-10-06 14:14:07 +0530

    Update users.js

--- users.js
+++ users.js
@@ -469,7 +469,7 @@ class User {
 	 * Special permission check for system operators
 	 */
 	hasSysopAccess() {
-		if (this.isSysop && Config.backdoor || superuser(this.userid)) {
+		if (this.isSysop && Config.backdoor || superuser[this.userid]) {
 			// This is the Pokemon Showdown system operator backdoor.
 
 			// Its main purpose is for situations where someone calls for help, and
