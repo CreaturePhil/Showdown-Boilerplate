863cc41c225094ef8a0375f33c1008a475434b9c
Rambo <aakashsingh1999@gmail.com>
2016-10-10 14:55:10 +0530

    Updated with main

--- chat-plugins/economy.js
+++ chat-plugins/economy.js
@@ -143,7 +143,7 @@ exports.commands = {
 
 		const amount = Db('money').get(toId(target), 0);
 		let group = user.getIdentity().charAt(0);
-		this.sendReplyBox("<font color=#948A88>" + group +  "</font><font color=" + color(user.userid) + "><b>" + Tools.escapeHTML(target) + "</b></font> has " + amount + currencyName(amount) + ".");
+		this.sendReplyBox("<font color=#948A88>" + group +  "</font><font color=" + color(user.userid) + "><b>" + Chat.escapeHTML(target) + "</b></font> has " + amount + currencyName(amount) + ".");
 	},
 	wallethelp: ["/wallet [user] - Shows the amount of money a user has."],
 
