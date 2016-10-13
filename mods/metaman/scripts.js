bee6a510b5ed2352e0ade939e4b611c89710f8f8
Spandan <spandan.punwatkar@gmail.com>
2016-10-13 15:54:15 +0530

    Fix Silly Mistakes

--- mods/metaman/scripts.js
+++ mods/metaman/scripts.js
@@ -5,14 +5,14 @@ exports.BattleScripts = {
 						this.lastMove = this.battle.getMove(move).id;
 						for(let i=0;i<this.moveset.length;i++)
 						{
-							if(this.lastMove == pokemon.moveset[i].id)
+							if(move.id == this.moveset[i].id)
 							{
-								this.lastM = pokemon.moveset[i];
+								this.lastM = this.moveset[i];
 								this.lastM.disabled = false;
 								this.lastM.pp = this.lastM.maxpp;
 								break;
 							}
-						} 
+						}
 						this.moveThisTurn = this.lastMove;
 				}
 		}
