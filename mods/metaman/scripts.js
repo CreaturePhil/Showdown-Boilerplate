e5209611732274e63ad6f8ea69e0f6c7d9f745e9
Spandan <spandan.punwatkar@gmail.com>
2016-10-13 15:37:06 +0530

    Fix Meta Man: The Final Round
    Which should fix it

--- mods/metaman/scripts.js
+++ mods/metaman/scripts.js
@@ -3,7 +3,7 @@ exports.BattleScripts = {
 		pokemon:{
 				moveUsed(move) {
 						this.lastMove = this.battle.getMove(move).id;
-						this.lastM = move;//this.baseMoveset[this.baseMoveset.indexOf(this.battle.getMove(move).id)];
+						this.lastM = this.baseMoveset[this.baseMoveset.indexOf(this.battle.getMove(move).id)];
 						this.lastM.pp = this.lastM.maxpp;
 						this.moveThisTurn = this.lastMove;
 				}
