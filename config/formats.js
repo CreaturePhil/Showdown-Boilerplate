5b36c369eb6956436dd014a02aad74de656522d2
Spandan <spandan.punwatkar@gmail.com>
2016-10-20 23:04:59 +0530

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -4436,7 +4436,7 @@ desc:["&bullet;<a href=\"http://www.smogon.com/forums/threads/recyclables.358181
 	     	],
 		section: "Experimental Metas",
 		mod: 'pokebilities',
-		team:"random",
+		//team:"random",
 		ruleset: ["Random Battle"],//'Sleep Clause Mod', 'Species Clause', 'OHKO Clause', 'Moody Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod', 'Team Preview'],
 		//banlist: ["Uber",'Unreleased', 'Shadow Tag', 'Soul Dew', "Assist", "Shedinja", "Huge Power", "Pure Power", 'Medichamite'],
 		onSwitchInPriority: 1,
