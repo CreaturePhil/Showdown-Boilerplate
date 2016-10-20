864db29e2ce48b1e2d1b4f18ce47312037321ccf
Spandan <spandan.punwatkar@gmail.com>
2016-10-20 22:23:04 +0530

    Update scripts.js

--- mods/pokebilities/scripts.js
+++ mods/pokebilities/scripts.js
@@ -4,19 +4,11 @@ exports.BattleScripts = {
 	init: function()
 	{
 			Object.values(this.data.Abilities).forEach(ability => {
-				let statusability = {"aerilate":true,"aurabreak":true,"flashfire":true,"parentalbond":true,"pixilate":true,"refrigerate":true,"sheerforce":true,"slowstart":true,"truant":true,"unburden":true,"zenmode":true}
-				if(statusability[ability.id])
-				{
-					this.data.Statuses[ability.id+"p"] = ability;
-					this.data.Statuses[ability.id+"p"].effectType = "Ability";
-					this.data.Statuses[ability.id+"p"].noCopy = true;
-				}
-				else
-				{
-					this.data.Statuses[ability.id] = ability;
-					this.data.Statuses[ability.id].effectType = "Ability";
-					this.data.Statuses[ability.id].noCopy = true;
-				}
+				this.data.Statuses["poke"+ability.id] = ability;
+				this.data.Statuses["poke"+ability.id] = "poke"+ability.id;
+				this.data.Statuses["poke"+ability.name] = "Pokebility "+ability.name;
+				this.data.Statuses["poke"+ability.id].effectType = "Ability";
+				this.data.Statuses["poke"+ability.id].noCopy = true;
 			});
 	},
 	pokemon: {
@@ -27,7 +19,7 @@ exports.BattleScripts = {
 			let item = (this.ignoringItem() ? '' : this.item);
 			if (item === 'ironball') return true;
 			if (!negateImmunity && this.hasType('Flying')) return false;
-			if ((this.hasAbility('levitate') || this.volatiles["levitate"]) && !this.battle.suppressingAttackEvents()) return null;
+			if ((this.hasAbility('levitate') || this.volatiles["pokelevitate"]) && !this.battle.suppressingAttackEvents()) return null;
 			if ('magnetrise' in this.volatiles) return false;
 			if ('telekinesis' in this.volatiles) return false;
 			return item !== 'airballoon';
