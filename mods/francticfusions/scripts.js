74a57bcdb4e5434ff44b345a4568e6db9e2f4ad7
Spandan <spandan.punwatkar@gmail.com>
2016-10-20 20:15:30 +0530

    Fix Levitate

--- mods/francticfusions/scripts.js
+++ mods/francticfusions/scripts.js
@@ -42,5 +42,17 @@ exports.BattleScripts = {
 			this.fusion = true;
 			return true;
 		},
+		isGrounded(negateImmunity) {
+			if ('gravity' in this.battle.pseudoWeather) return true;
+			if ('ingrain' in this.volatiles) return true;
+			if ('smackdown' in this.volatiles) return true;
+			let item = (this.ignoringItem() ? '' : this.item);
+			if (item === 'ironball') return true;
+			if (!negateImmunity && this.hasType('Flying')) return false;
+			if ((this.hasAbility('levitate') || this.volatiles["levitate"]) && !this.battle.suppressingAttackEvents()) return null;
+			if ('magnetrise' in this.volatiles) return false;
+			if ('telekinesis' in this.volatiles) return false;
+			return item !== 'airballoon';
+		}
 	},
 };
