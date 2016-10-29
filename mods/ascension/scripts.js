3a7e7de6d9b5bcab607f5d8604e6613ec6189588
Snaquaza <Snaquaza@Dragonfield.com>
2016-10-29 14:52:24 +0200

    Create scripts.js

---
+++ mods/ascension/scripts.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.BattleScripts = {
+	init: function () {
+		this.modData('Pokedex', 'samurott').types = ['Water', 'Fighting'];
+		this.modData('Learnsets', 'samurott').learnset.closecombat = ['6L100'];
+		this.modData('Learnsets', 'samurott').learnset.focusblast = ['6M'];
+		this.modData('Learnsets', 'samurott').learnset.shellsmash = ['6L100'];
+    
+    this.modData('Pokedex', 'arcanine').abilities['H'] = 'Desolate Land';
+		}
+	},
+};
