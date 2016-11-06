37cf1b14a07fe7fd94deab5be25bef36b1d5629f
Spandan <spandan.punwatkar@gmail.com>
2016-11-06 15:52:13 +0530

    Update scripts.js

--- mods/pokebilities/scripts.js
+++ mods/pokebilities/scripts.js
@@ -21,6 +21,39 @@ exports.BattleScripts = {
 					this.data.Statuses[ability.id].effectType = "Ability";
 					this.data.Statuses[ability.id].noCopy = true;
 				}
+				this.data.Statuses.trace = {
+					desc: "On switch-in, this Pokemon copies a random adjacent opposing Pokemon's Ability. If there is no Ability that can be copied at that time, this Ability will activate as soon as an Ability can be copied. Abilities that cannot be copied are Flower Gift, Forecast, Illusion, Imposter, Multitype, Stance Change, Trace, and Zen Mode.",
+					shortDesc: "On switch-in, or when it can, this Pokemon copies a random adjacent foe's Ability.",
+					onUpdate: function (pokemon) {
+						let possibleTargets = [];
+						for (let i = 0; i < pokemon.side.foe.active.length; i++) {
+							if (pokemon.side.foe.active[i] && !pokemon.side.foe.active[i].fainted) possibleTargets.push(pokemon.side.foe.active[i]);
+						}
+						while (possibleTargets.length) {
+							let rand = 0;
+							if (possibleTargets.length > 1) rand = this.random(possibleTargets.length);
+							let target = possibleTargets[rand];
+							let ability = this.getAbility(target.abilitwo);
+							let bannedAbilities = {flowergift:1, forecast:1, illusion:1, imposter:1, multitype:1, stancechange:1, trace:1, zenmode:1};
+							if (bannedAbilities[ability]) {
+								possibleTargets.splice(rand, 1);
+								continue;
+							}
+							this.add('-ability', pokemon, ability, '[from] ability: Trace', '[of] ' + target);
+							let statusability = {"aerilate":true,"aurabreak":true,"flashfire":true,"parentalbond":true,"pixilate":true,"refrigerate":true,"sheerforce":true,"slowstart":true,"truant":true,"unburden":true,"zenmode":true};
+							let sec = statusability[pokemon.abilitwo]? "other"+pokemon.abilitwo : pokemon.abilitwo;
+							pokemon.removeVolatile("trace", pokemon);
+							pokemon.addVolatile(sec, pokemon);
+							return;
+						}
+					},
+					id: "trace",
+					name: "Trace",
+					rating: 3,
+					num: 36,
+					effectType: "Ability", 
+					noCopy: true,
+				};
 			});
 	},
 	pokemon: {
