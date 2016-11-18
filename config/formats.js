59a334e1c8c3dd08221b4fc22f158baecf3d7e0e
Spandan <spandan.punwatkar@gmail.com>
2016-11-18 16:01:44 +0530

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -4651,7 +4651,7 @@ desc:["&bullet;<a href=\"http://www.smogon.com/forums/threads/recyclables.358181
 				pokemon.addVolatile(sec, pokemon);//Second Ability! YAYAYAY
 			}
 		},
-		validateSet(set, teamHas) { 
+		validateSet: function(set, teamHas) { 
 			let item = set.item;
 			if(this.getAbility(item))
 			{
