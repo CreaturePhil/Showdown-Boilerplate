c2d39c67627722949e8b9b106c421f7f087dcf26
Snaquaza <Snaquaza@Dragonfield.com>
2016-11-26 13:09:46 +0100

    Update pokedex.js

--- mods/aurora/pokedex.js
+++ mods/aurora/pokedex.js
@@ -1,1 +1,15 @@
-nothing yet
+'use strict';
+
+exports.BattlePokedex = {
+  		yddraig: {
+	      	num: 10001,
+	      	species: "Yddraig",
+	      	types: ["Dragon"],
+		      baseStats: {hp: 60, atk: 60, def: 55, spa: 75, spd: 55, spe: 85},
+	      	abilities: {0: "Infernal Scales", H: "Shed Skin"},
+	      	heightm: 1.1,
+	      	weightkg: 23,
+	      	color: "Red",
+	      	eggGroups: ["Dragon"],
+    	},
+  };
