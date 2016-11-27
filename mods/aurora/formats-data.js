985d4b04cbde8f578e5f8b29a7957144f4c580a6
Snaquaza <Snaquaza@Dragonfield.com>
2016-11-27 10:51:53 +0100

    Update formats-data.js

--- mods/aurora/formats-data.js
+++ mods/aurora/formats-data.js
@@ -5,4 +5,8 @@ exports.BattleFormatsData = {
 		randomBattleMoves: ["fireblast", "clangingscales", "defog", "roost"],
 		tier: "New",
 	},
+	detuoy: {
+		randomBattleMoves: ["explosion"],
+		tier: "New",
+	},
 };
