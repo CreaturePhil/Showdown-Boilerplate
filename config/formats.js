696ea855fe09440fdc595474d2345de6a3388054
Spandamn <Spandamn>
2016-12-04 14:25:44 +0530

    Test

--- config/formats.js
+++ config/formats.js
@@ -4462,20 +4462,11 @@ desc:["&bullet;<a href=\"http://www.smogon.com/forums/threads/recyclables.358181
 	},
 	{
 		name: "Beastly Terrain",
+		mod: "franticfusions",
 		ruleset: ['Random Battle'],
 		team: 'random',
-		onSourceFaint: function (target, source, effect) {
-			if (effect && effect.effectType === 'Move') {
-				let stat = 'atk';
-				let bestStat = 0;
-				for (let i in source.stats) {
-					if (source.stats[i] > bestStat) {
-						stat = i;
-						bestStat = source.stats[i];
-					}
-				}
-				this.boost({[stat]:1}, source);
-			}
+		onSwitchIn: function (pokemon) {
+			pokemon.addVolatile("beastboost");
 		},
 	},
 	{
