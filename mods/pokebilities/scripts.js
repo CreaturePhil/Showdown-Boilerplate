028ba1f818cbe6620092c78721e8358f7f3a5007
Spandamn <Spandamn>
2016-12-05 15:42:01 +0530

    Fix2

--- mods/pokebilities/scripts.js
+++ mods/pokebilities/scripts.js
@@ -21,9 +21,19 @@ exports.BattleScripts = {
 					this.data.Statuses[ability.id].effectType = "Ability";
 					this.data.Statuses[ability.id].noCopy = true;
 				}
-				this.data.Statuses.["moldbreaker"].onModifyMove = function(move) {
-					move.ignoreAbility = true;
-				} 
+				this.data.Statuses.["moldbreaker"] = {
+						shortDesc: "This Pokemon's moves and their effects ignore the Abilities of other Pokemon.",
+						onStart: function (pokemon) {
+							this.add('-ability', pokemon, 'Mold Breaker');
+						},
+						onModifyMove: function(move) {
+							move.ignoreAbility = true;
+						}
+						id: "moldbreaker",
+						name: "Mold Breaker",
+						rating: 3.5,
+						num: 104,
+				};
 				this.data.Statuses.trace = {
 					desc: "On switch-in, this Pokemon copies a random adjacent opposing Pokemon's Ability. If there is no Ability that can be copied at that time, this Ability will activate as soon as an Ability can be copied. Abilities that cannot be copied are Flower Gift, Forecast, Illusion, Imposter, Multitype, Stance Change, Trace, and Zen Mode.",
 					shortDesc: "On switch-in, or when it can, this Pokemon copies a random adjacent foe's Ability.",
