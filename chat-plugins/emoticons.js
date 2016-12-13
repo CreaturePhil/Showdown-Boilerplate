7da746b46e472f2ec64bac31685a1e183d3f31de
Spandan <spandan.punwatkar@gmail.com>
2016-12-13 21:40:26 +0530

    Update emoticons.js

--- chat-plugins/emoticons.js
+++ chat-plugins/emoticons.js
@@ -104,7 +104,6 @@ if (typeof demFeels.extendEmotes === 'function') {
 		'llamaconfuse':'http://orig01.deviantart.net/7863/f/2013/347/1/6/llama_emoji_38__confused___v2__by_jerikuto-d6uwuuu.gif',
 		'xoxo':'http://i.imgur.com/Yyw9ICj.png',
 		'feelsxp': 'http://i.imgur.com/sTXEtjS.jpg',
-		'feels13': "http://i.imgur.com/eWQDphf.jpg",
 	});
 }
 
