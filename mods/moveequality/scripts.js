d355609f545ece4e84b69232b715156f15aa9611
charizard8888 <omkumr082001@outlook.com>
2016-12-13 15:26:58 +0530

    Move Equality 2.0 Code
    Don't merge now

--- mods/moveequality/scripts.js
+++ mods/moveequality/scripts.js
@@ -1,34 +1,29 @@
 exports.BattleScripts = {
     init: function () {
-        for (var i in this.data.Moves) {
-            if (this.data.Moves[i].priority === 0) {
-                var basePower = this.data.Moves[i].basePower;
-                switch (this.data.Moves[i].multihit) {
+        for (var i in this.data.Movedex) {
+            var move = this.data.Movedex[i];
+            if (move.priority === 0
+                && move.category !== 'Status') {
+                var basePower = move.basePower;
+                switch (move.multihit) {
                     case 2:
                         basePower = 45;
                     break;
-                    case 3:
-                        basePower = 15;
-                    break;
                     case [2, 5]:
                         basePower = 25;
+                    break;
+                    case 3:
+                        basePower = 30;
                     break;
                     default:
                         basePower = 90;
                     break;
                 }
-            
-                var basePowerCallback;
-                if (this.data.Moves[i].id === 'triplekick') {
-                    basePowerCallback = function (pokemon) {
-                        pokemon.addVolatile('triplekick');
-                        return 15 * pokemon.volatiles['triplekick'].hit;
-                    }
-                }
-                this.modData('Moves', i).basePower = basePower;
-                this.modData('Moves', i).accuracy = 100;
-                if (basePowerCallback) this.modData('Moves', i).basePowerCallback = basePowerCallback;
-                this.modData('Moves', i).onBasePower = undefined;
+              
+                this.modData('Movedex', i).basePower = basePower || 90;
+                this.modData('Movedex', i).accuracy = 100;
+                this.modData('Movedex', i).basePowerCallback = undefined;
+                this.modData('Movedex', i).onBasePower = undefined;
             }
         }
     }
