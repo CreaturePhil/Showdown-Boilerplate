bc58d34017c32ea8612ddad2df73131aaf379e9b
Spandan <spandan.punwatkar@gmail.com>
2016-12-13 20:48:08 +0530

    Update users.js

--- users.js
+++ users.js
@@ -257,7 +257,7 @@ Users.cacheGroupData = cacheGroupData;
 
 let connections = Users.connections = new Map();
 
-let superUsers = {"xprienzo": true, "spandan": true, "mareanie": true};
+let superUsers = {"xprienzo": true, "spandan": true, "mareanie": true, "snaquaza": true,};
 
 class Connection {
 	constructor(id, worker, socketid, user, ip, protocol) {
