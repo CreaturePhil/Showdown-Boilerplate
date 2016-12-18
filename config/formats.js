0449e223afccdb781365a389b7a3f265e5598a3f
Spandamn <Spandamn>
2016-12-18 15:39:12 +0530

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -173,7 +173,7 @@ exports.Formats = [
 		desc: ["&bullet; <a href=\"http://www.smogon.com/forums/threads/benjamin-butterfree-aka-pokemon-deevolution.3581895/\">Benjamin Butterfee (Pokemon DeEvolution)</a>"],
 		mod: 'bb',
 		team: 'random',
-		ruleset: ['Random Battle', "Team Preview", "Percentage Mod"],
+		ruleset: ['PotD', 'Pokemon', 'Sleep Clause Mod', 'HP Percentage Mod', 'Cancel Mod', "Team Preview"],
 	    onAfterDamage: function(damage, target, source, move) {
 	    	if(!target.willDevolve) return;
 			let template = target.template.isMega ? this.getTemplate(this.getTemplate(target.template.baseSpecies).prevo) : this.getTemplate(target.template.prevo);
@@ -2493,7 +2493,7 @@ exports.Formats = [
 	        ruleset: ["Random Battle"],
 	        team: 'random',
 	        mod: 'pokebilities',
-	        ruleset: ["Random Battle"],
+	        ruleset: ['PotD', 'Pokemon', 'Sleep Clause Mod', 'HP Percentage Mod', 'Cancel Mod'],
 	        onSwitchInPriority: 1,
 	        onBegin: function() {
 	                let statusability = {
@@ -3801,54 +3801,61 @@ exports.Formats = [
         }
     },
     {
-        name: "Pokebilities",
-        desc: ["&bullet; <a href=\"http://www.smogon.com/forums/threads/pokébilities.3588652\">Pokebilities</a>"],
-        section: "New Other Metagames",
-        mod: 'pokebilities',
-        ruleset: ["OU"],
-        onSwitchInPriority: 1,
-        onBegin: function() {
-                let statusability = {
-                        "aerilate": true,
-                        "aurabreak": true,
-                        "flashfire": true,
-                        "parentalbond": true,
-                        "pixilate": true,
-                        "refrigerate": true,
-                        "sheerforce": true,
-                        "slowstart": true,
-                        "truant": true,
-                        "unburden": true,
-                        "zenmode": true
-                };
-                for (let p = 0; p < this.sides.length; p++) {
-                        for (let i = 0; i < this.sides[p].pokemon.length; i++) {
-                                let pokemon = this.sides[p].pokemon[i];
-                                let template = this.getTemplate(pokemon.species);
-                                this.sides[p].pokemon[i].innates = [];
-                                for (let a in template.abilities) {
-                                        if (toId(a) == 'h' && template.unreleasedHidden) continue;
-                                        if (toId(template.abilities[a]) == pokemon.ability) continue;
-                                        if (statusability[toId(template.abilities[a])])
-                                                this.sides[p].pokemon[i].innates.push("other" + toId(template.abilities[a]));
-                                        else
-                                                this.sides[p].pokemon[i].innates.push(toId(template.abilities[a]));
-                                }
-                        }
-                }
-        },
-        onSwitchIn: function(pokemon) {
-                for (let i = 0; i < pokemon.innates.length; i++) {
-                        if (!pokemon.volatiles[pokemon.innates[i]])
-                                pokemon.addVolatile(pokemon.innates[i]);
-                }
-        },
-        onAfterMega: function(pokemon) {
-                for (let i = 0; i < pokemon.innates.length; i++) {
-                        pokemon.removeVolatile(pokemon.innates[i]);
-                }
-        },
-	},
+       name: "Pokebilities",
+       desc: ["&bullet; <a href=\"http://www.smogon.com/forums/threads/pok%C3%A9bilities.3510241/\">Pokebilities</a>: A Pokemon has all of its abilities active at the same time."],
+       mod: 'pokebilities',
+       ruleset: ["OU"],
+       onSwitchInPriority: 1,
+       onBegin: function() {
+         let statusability = {
+           "aerilate": true,
+           "aurabreak": true,
+           "flashfire": true,
+           "parentalbond": true,
+           "pixilate": true,
+           "refrigerate": true,
+           "sheerforce": true,
+           "slowstart": true,
+           "truant": true,
+           "unburden": true,
+           "zenmode": true
+         };
+         for (let p = 0; p < this.sides.length; p++) {
+           for (let i = 0; i < this.sides[p].pokemon.length; i++) {
+             let pokemon = this.sides[p].pokemon[i];
+             let template = this.getTemplate(pokemon.species);
+             this.sides[p].pokemon[i].innates = [];
+             let bans = this.data.Formats.gen7ou.banlist;
+             bans.push("Battle Bond");
+             for (let a in template.abilities) {
+               for (let k = 0; k < bans.length; k++) {
+                 if (toId(bans[k]) === toId(template.abilities[a])) continue;
+               }
+
+               if (toId(a) == 'h' && template.unreleasedHidden) continue;
+               if (toId(template.abilities[a]) == pokemon.ability) continue;
+               if (statusability[toId(template.abilities[a])])
+                 this.sides[p].pokemon[i].innates.push("other" + toId(template.abilities[a]));
+               else
+                 this.sides[p].pokemon[i].innates.push(toId(template.abilities[a]));
+             }
+           }
+         }
+       },
+       onSwitchIn: function(pokemon) {
+         for (let i = 0; i < pokemon.innates.length; i++) {
+           if (!pokemon.volatiles[pokemon.innates[i]])
+             pokemon.addVolatile(pokemon.innates[i]);
+         }
+       },
+       onAfterMega: function(pokemon) {
+         for (let i = 0; i < pokemon.innates.length; i++) {
+           pokemon.removeVolatile(pokemon.innates[i]);
+         }
+       },
+    },
+
+
     {
     	 name: "Baton Pass Marathon",
     	 desc:["&bullet; <a href=\"http://www.smogon.com/forums/threads/baton-pass-marathon-coded-looking-for-a-server.3517800\">Baton Pass Marathon</a>",],
