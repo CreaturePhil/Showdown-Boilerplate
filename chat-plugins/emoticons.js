73dec75c5d85c80e07d3ed20e8123bd032d0d4b9
charizard8888 <omkumr082001@outlook.com>
2016-12-25 17:22:01 +0530

    More emotes lel

--- chat-plugins/emoticons.js
+++ chat-plugins/emoticons.js
@@ -104,6 +104,10 @@ if (typeof demFeels.extendEmotes === 'function') {
 		'llamaconfuse':'http://orig01.deviantart.net/7863/f/2013/347/1/6/llama_emoji_38__confused___v2__by_jerikuto-d6uwuuu.gif',
 		'xoxo':'http://i.imgur.com/Yyw9ICj.png',
 		'feelsxp': 'http://i.imgur.com/sTXEtjS.jpg',
+		'feelsfab': 'http://i.imgur.com/vCPgsQA.jpg',
+		'eeveelutions!':'http://2.media.dorkly.cvcdn.com/25/38/4d9df0e6985ab9e9218f3874ccf9649d.gif',
+		'feelslucario':'http://i.imgur.com/ZQbYp9l.gif',
+		
 	});
 }
 
