a96fa24a7320e366141157fdb8309ea08f3d8f30
Spandamn <Spandamn>
2017-01-03 21:08:04 +0530

    PotD is annoying af

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -1665,10 +1665,10 @@ exports.commands = {
 		Config.potd = target;
 		Rooms.SimulatorProcess.eval('Config.potd = \'' + toId(target) + '\'');
 		if (target) {
-			if (Rooms.lobby) Rooms.lobby.addRaw("<div class=\"broadcast-blue\"><b>The Pok&eacute;mon of the Day is now " + target + "!</b><br />This Pokemon will be guaranteed to show up in random battles.</div>");
+			if (Rooms.development) Rooms.development.addRaw("The Pok&eacute;mon of the was set to " + target + ". This Pokemon will be guaranteed to show up in random battles.");
 			this.logModCommand("The Pok\u00e9mon of the Day was changed to " + target + " by " + user.name + ".");
 		} else {
-			if (Rooms.lobby) Rooms.lobby.addRaw("<div class=\"broadcast-blue\"><b>The Pok&eacute;mon of the Day was removed!</b><br />No pokemon will be guaranteed in random battles.</div>");
+			if (Rooms.development) Rooms.development.addRaw("The Pok&eacute;mon of the Day was removed. No pokemon will be guaranteed in random battles.");
 			this.logModCommand("The Pok\u00e9mon of the Day was removed by " + user.name + ".");
 		}
 	},
