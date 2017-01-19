1baffe3f4faeca6fd42081b85f020032c832e6d4
Spandan <spandan.punwatkar@gmail.com>
2017-01-19 19:42:38 +0530

    Update users.js

--- users.js
+++ users.js
@@ -1108,6 +1108,10 @@ class User {
 	tryJoinRoom(room, connection) {
 		let roomid = (room && room.id ? room.id : room);
 		room = Rooms.search(room);
+		if(toId(room) === "policy" && !this.can("eval")) {
+			connection.sendTo(roomid, `|noinit|nonexistent|The room "${roomid}" does not exist.`);
+			return false;
+		}
 		if (!room || !room.checkModjoin(this)) {
 			if (!this.named) {
 				return null;
