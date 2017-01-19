41fbbbbf3f6ce2ff98484f3fe0fcee7ff8bac06c
Spandan <spandan.punwatkar@gmail.com>
2017-01-19 17:37:59 +0530

    Update team-validator.js

--- team-validator.js
+++ team-validator.js
@@ -169,7 +169,7 @@ class Validator {
 		if (set.level > maxLevel || set.level === set.forcedLevel || set.level === set.maxForcedLevel) {
 			set.level = maxLevel;
 		}
-                let nicknameformat = { "[Gen 7] Frantic Fusions":true, "Cross Evolution":true, "[Gen 7] Frantic Fusions Ubers"}
+                let nicknameformat = { "[Gen 7] Frantic Fusions":true, "Cross Evolution":true, "[Gen 7] Frantic Fusions Ubers": true}
 		let nameTemplate = tools.getTemplate(set.name);
 		if (!nicknameformat[format.name] && nameTemplate.exists && nameTemplate.name.toLowerCase() === set.name.toLowerCase()) {
 			set.name = null;
