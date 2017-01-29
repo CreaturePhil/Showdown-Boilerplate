b3bf22e6f342642e797c80da279ad69e043ae752
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2017-01-29 21:51:10 +0530

    Update scripts.js

--- mods/istor/scripts.js
+++ mods/istor/scripts.js
@@ -11,9 +11,9 @@ exports.BattleScripts = {
 		return cat || 'Physical';
 	},
 	randomFotWTeam: function (side) {
-		let fotw = this.getFormat().fotw;
-		do {
-			let team = this.randomTeam(side);
+		let fotw = this.getFormat().fotw, team = this.randomTeam(side);
+		do {			
+			team = this.randomTeam(side);
 			team.length = 5;
 			for(let i =0;i<5;i++) 
 				if(team[i].species === fotw) continue;
