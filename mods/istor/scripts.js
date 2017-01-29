4ffab1e03cc7702cf2f23db1d735435926aef3b1
Snaquaza <Snaquaza@Dragonfield.com>
2017-01-29 21:27:29 +0100

    Update scripts.js

--- mods/istor/scripts.js
+++ mods/istor/scripts.js
@@ -10,17 +10,4 @@ exports.BattleScripts = {
 		}
 		return cat || 'Physical';
 	},
-	randomFotWTeam: function (side) {
-		let fotw = this.getFormat().fotw, team = this.randomTeam(side);
-		do {			
-			team = this.randomTeam(side);
-			team.length = 5;
-			for(let i =0;i<5;i++) 
-				if(team[i].species === fotw) continue;
-			break;
-		}
-		while(true);
-		team.push(this.randomSet(this.getTemplate(fotw)));
-		return team;
-	},
 };
