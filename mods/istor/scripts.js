a898cccb69ac3910467a48a410f4cb128994d4eb
Spandamn <Spandamn>
2017-01-29 16:52:35 +0530

    Add FotW

--- mods/istor/scripts.js
+++ mods/istor/scripts.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.BattleScripts = {
-	getCategory(move) {
+	getCategory: function(move) {
 		move = this.getMove(move);
 		let cat = move.category;
 		if(this.pseudoWeather["wonderroom"]) {
@@ -9,5 +9,18 @@ exports.BattleScripts = {
 			if(cat === "Physical") return "Special";
 		}
 		return cat || 'Physical';
-	}
+	},
+	randomFotWTeam: function (side) {
+		let fotw = this.getFormat().fotw;
+		do {
+			let team = this.randomTeam(side);
+			team.length = 5;
+			for(let i =0;i<5;i++) 
+				if(team[i].species === fotw) continue;
+			break;
+		}
+		while(true);
+		team.push(randomSet(this.getTemplate(fotw)));
+		return team;
+	},
 };
