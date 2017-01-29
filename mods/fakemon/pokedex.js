d927f0387c3b0e131cf6116bfff5c48c2e135333
Snaquaza <Snaquaza@Dragonfield.com>
2017-01-29 21:35:16 +0100

    Create new file

---
+++ mods/fakemon/pokedex.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports.BattlePokedex = {
+ 	solsprite: {
+	      	num: 999,
+	      	species: "Solsprite",
+	      	types: ["Fire", "Fairy"],
+		genderRatio: {M: 0.5, F: 0.5},
+		baseStats: {hp: 100, atk: 70, def: 130, spa: 110, spd: 90, spe: 80},
+	      	abilities: {0: "Serene Grace"},
+	      	heightm: 100,
+	      	weightkg: 100,
+	      	color: "Red",
+	      	eggGroups: ["Field"],
+    	},
+};
