a03da9442b640b0dad4bee7ae3fe503494ecf98e
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2017-02-03 20:01:04 +0530

    Update chat-commands.js

--- chat-commands.js
+++ chat-commands.js
@@ -2347,7 +2347,7 @@ exports.commands = {
 	
 	//Roomlog code by Spandan
 	roomlog: function (target, room, user, connection) {
-		if(!Config.logchat) return;
+		if(!Config.logchat) return this.errorReply("Roomlogs are disabled for this server. (Did you mean: modlog?)");
 		if(!this.can("roomdriver")) return;
 		if(!target || target === "") return this.errorReply("Error: Please provide a parameter.");
 		let date = target.trim(), month = date.substring(0,7);
