a8f8e00f5c5cf7674c5605d29a0b8a6cf7731b70
charizard8888 <omkumr082001@outlook.com>
2017-02-10 15:45:25 +0530

    PTFNG

---
+++ mods/thefirstnewgen/pokedex.js
@@ -0,0 +1,18 @@
+'use strict';
+
+exports.BattlePokedex = {
+    tubi: {
+	      	num: 8000,
+	      	species: "Tubi",
+	      	types: ["Grass"],
+		      baseStats: {hp: 43, atk: 70, def: 31, spa: 55, spd: 55, spe: 64},
+	      	abilities: {0: "Overgrow", H: "Moxie"},
+    	},
+      shruber: {
+	      	num: 8001,
+	      	species: "Shrubber",
+	      	types: ["Grass","Ground"],
+		      baseStats: {hp: 55, atk: 85, def: 45, spa: 79, spd: 65, spe: 86},
+	      	abilities: {0: "Overgrow", H: "Moxie"},
+    	},
+};
