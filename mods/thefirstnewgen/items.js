8e4fe370c6b3ec6fb83ac9fc97b96c68e983249d
charizard8888 <omkumr082001@outlook.com>
2017-02-11 13:05:06 +0530

    Coded all the items except Adaptibility Orb

--- mods/thefirstnewgen/items.js
+++ mods/thefirstnewgen/items.js
@@ -32,4 +32,63 @@ exports.BattleItems = {
         zMoveFrom: "Antler Scorch",
         zMoveUser: ["Pyromoose"], 
       },    
+        "allrounderbadge": { /* Not Sure about this :P */
+		id: "allrounderbadge",
+		name: "All-Rounder Badge",
+		onModifyAtkPriority: 1,
+		onModifyAtk: function (atk) {
+			return this.chainModify(1.05);
+		},
+                onModifyDefPriority: 1,
+		onModifyDef: function (def) {
+			return this.chainModify(1.05);
+		},
+                onModifySpAPriority: 1,
+		onModifySpA: function (spa) {
+			return this.chainModify(1.05);
+		},
+                onModifySpDPriority: 1,
+		onModifySpD: function (spd) {
+			return this.chainModify(1.05);
+		},
+                onModifySpePriority: 1,
+		onModifySpe: function (spe) {
+			return this.chainModify(1.05);
+		},
+                
+	},
+        "assaultshield": {
+		id: "assaultshield",
+		name: "assault Shield",
+		onModifyDefPriority: 1,
+		onModifyDef: function (def) {
+			return this.chainModify(1.5);
+		},
+		onDisableMove: function (pokemon) {
+			let moves = pokemon.moveset;
+			for (let i = 0; i < moves.length; i++) {
+				if (this.getMove(moves[i].move).category === 'Status') {
+					pokemon.disableMove(moves[i].id);
+				}
+			}
+		},
+                /* Adaptibility Orb */
+           "moonshard": {
+		id: "moonshard",
+		name: "Moon Shard",
+		spritenum: 610,
+		onPlate: 'Fairy',
+		onBasePowerPriority: 6,
+		onBasePower: function (basePower, user, target, move) {
+			if (move && move.type === 'Fairy') {
+				return this.chainModify([0x1333, 0x1000]);
+			}
+		},
+		onTakeItem: function (item, pokemon, source) {
+			if ((source && source.baseTemplate.num === 493) || pokemon.baseTemplate.num === 493) {
+				return false;
+			}
+			return true;
+		},
+	},
 };
