774f8e720cff4bde1b118091fd2d35b7df13acfd
charizard8888 <omkumr082001@outlook.com>
2017-02-11 12:41:19 +0530

    Create items.js

---
+++ mods/thefirstnewgen/items.js
@@ -0,0 +1,35 @@
+'use strict';
+exports.BattleItems = {
+        "fungariumz": {
+        id: "fungariumz",
+        name: "Fungarium-Z",
+        onTakeItem: false,
+        zMove: "7-Slash Bushido Assault",
+        zMoveFrom: "Earthen Katana",
+        zMoveUser: ["Fungarai"],
+      },
+      "steelicilliumz": {
+        id: "steelicilliumz",
+        name: "Steelicillium-Z",
+        onTakeItem: false,
+        zMove: "Molten Steel Blast",
+        zMoveFrom: "Scorch Shot",
+        zMoveUser: ["Steelicillo"], 
+      },
+      "dorshelliumz": {
+        id: "dorshelliumz",
+        name: "Dorshellium-Z",
+        onTakeItem: false,
+        zMove: "Petrifying Glacier Charge",
+        zMoveFrom: "Arctic Blade",
+        zMoveUser: ["Dorshellical"], 
+      },
+      "pyrominiumz": {
+        id: "pyrominiumz",
+        name: "Pyrominium-Z",
+        onTakeItem: false,
+        zMove: "Incinerating Antler Lunge",
+        zMoveFrom: "Antler Scorch",
+        zMoveUser: ["Pyromoose"], 
+      },    
+};
