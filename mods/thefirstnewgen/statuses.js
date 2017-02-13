257af1a96782bb843011a103f1967b8ba70d4dd6
charizard8888 <omkumr082001@outlook.com>
2017-02-13 16:46:29 +0530

    Add Infected & Acid Rain (not sure about these :p)

---
+++ mods/thefirstnewgen/statuses.js
@@ -0,0 +1,48 @@
+'use strict';
+
+exports.BattleStatuses = {
+	    infected: { /* Get 1/2 X SpA working */
+		              effectType: 'Status',
+                  onStart: function (target, source, sourceEffect) {
+                    } else if (sourceEffect && sourceEffect.effectType === 'Ability') {
+                      this.add('-status', target, 'infected', '[from] ability: ' + sourceEffect.name, '[of] ' + source);
+                    } else {
+                      this.add('-status', target, 'infected');
+                    }
+                  },
+                  onResidualOrder: 9,
+                  onResidual: function (pokemon) {
+                    this.damage(pokemon.maxhp / 16);
+                  },
+              },
+       acidrain: {
+		effectType: 'Weather',
+		duration: 5,
+		durationCallback: function (source, effect) {
+			if (source && source.hasItem('necrosis')) {
+				return 8;
+			}
+			return 5;
+		},
+		onStart: function (battle, source, effect) {
+			if (effect && effect.effectType === 'Ability') {
+				if (this.gen <= 5) this.effectData.duration = 0;
+				this.add('-weather', 'AcidRain', '[from] ability: ' + effect, '[of] ' + source);
+			} else {
+				this.add('-weather', 'AcidRain');
+			}
+		},
+		onResidualOrder: 1,
+		onResidual: function () {
+			this.add('-weather', 'AcidRain', '[upkeep]');
+			if (this.isWeather('acidrain')) this.eachEvent('Weather');
+		},
+		onWeather: function (target) {
+			this.damage(target.maxhp / 16);
+		},
+		onEnd: function () {
+			this.add('-weather', 'none');
+		},
+	},
+  
+};
