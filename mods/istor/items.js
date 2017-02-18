99374140d9c6de5ffbc1753cd7b290d3ec46c560
Snaquaza <Snaquaza@Dragonfield.com>
2017-02-18 17:39:29 +0100

    Create items.js

---
+++ mods/istor/items.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.BattleItems = {
+	"toxicrock": {
+		id: "toxicrock",
+		name: "Toxic Rock",
+		spritenum: 88,
+		fling: {
+			basePower: 60,
+		},
+		num: 285,
+		gen: 4,
+		desc: "Holder's use of Noxious Fumes lasts 8 turns instead of 5.",
+    },
+  };
