6db5281dd796e9b97227c82dfc6a7a828517535b
Snaquaza <Snaquaza@Dragonfield.com>
2017-02-20 20:50:28 +0100

    Update items.js

--- mods/istor/items.js
+++ mods/istor/items.js
@@ -8,8 +8,22 @@ exports.BattleItems = {
 		fling: {
 			basePower: 60,
 		},
-		num: 285,
-		gen: 4,
+		num: 10001,
+		gen: 7,
 		desc: "Holder's use of Noxious Fumes lasts 8 turns instead of 5.",
+	},
+	"snowboard": {
+		id: "snowboard",
+		name: "Snowboard",
+		spritenum: 69,
+		fling: {
+			basePower: 40,
+		},
+		onModifySpe: function (spe) {
+			if (this.isWeather('hail')) return this.chainModify(1.5);
+		},
+		num: 10002,
+		gen: 7,
+		desc: "Holder's Speed is 1.5x, but only during a hailstorm.",
 	},
 };
