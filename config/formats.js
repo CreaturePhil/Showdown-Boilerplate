7d17b76ce9d0a894ff41af4026fcf4b28d7b7c85
Snaquaza <Snaquaza@Dragonfield.com>
2017-03-12 20:50:27 +0100

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -5888,6 +5888,6 @@ exports.Formats = [
 		mod: 'fakemon',
 		team: 'randomFotW',
 		ruleset: ['Pokemon', 'Sleep Clause Mod', 'HP Percentage Mod', 'Cancel Mod', 'Freeze Clause Mod'],
-		fotw: "Jawnado",
+		fotw: "Valkyrieti",
 	},
 ];
