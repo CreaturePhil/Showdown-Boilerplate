07e987fdb9fc8d4b247627733e339661638ff0f2
Spandamn <Spandamn>
2017-03-19 14:02:53 +0530

    Fix /crossevo

--- chat-plugins/othermetas.js
+++ chat-plugins/othermetas.js
@@ -8,7 +8,7 @@ exports.commands = {
 	{
 		if (!this.runBroadcast()) return;
 		if (!target || !target.includes(',')) return this.parse('/help crossevo')
-		let pokes = arg.split(",");
+		let pokes = target.split(",");
 		if (!Tools.data.Pokedex[pokes[0]] || !Tools.data.Pokedex[pokes[1]]) {
 			return this.errorReply('Error: Pokemon not found.')
 		}
