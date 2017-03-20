7fa610e66608b0b10960146119566df623071277
Spandamn <Spandamn>
2017-03-20 14:19:39 +0530

    Completely serverside....

--- tools.js
+++ tools.js
@@ -683,6 +683,27 @@ class BattleDex {
 		return `<div class="message"><ul class="utilichart">${buf}<li style="clear:both"></li></ul></div>`;
 	}
 
+	getDataMoveHTML(move) {
+		if (typeof move === 'string') move = Tools.getMove(move);
+		let buf = `<ul class="utilichart"><li class="result">`;
+		buf += ` <a data-entry="move|${move.name}"><span class="col movenamecol">${move.name}</span>`;
+		buf += ` <span class="col typecol"><img src="//play.pokemonshowdown.com/sprites/types/${move.type}.png" alt="${move.type}" width="32" height="14">`;
+		buf += ` <img src="//play.pokemonshowdown.com/sprites/categories/${move.category}.png" alt="${move.category}" width="32" height="14"></span>`;
+		buf += ` <span class="col labelcol"></span> <span class="col widelabelcol"><em>Accuracy</em><br>${typeof move.accuracy === 'number' ? move.accuracy : '—'}</span>`;
+		buf += ` <span class="col pplabelcol"><em>PP</em><br>${move.pp}</span> <span class="col movedesccol">${move.shortDesc}</span>`;
+		buf += ` </a></li><li style="clear:both"></li></ul>`;
+		return buf;
+	}
+
+	getDataAbilityHTML(ability) {
+		if (typeof ability === 'string') ability = Tools.getAbility(ability);
+		let buf = `<ul class="utilichart"><li class="result">`;
+		buf += `<a data-entry="ability|${ability.name}"><span class="col namecol">${ability.name}</span> `;
+		buf += `<span class="col abilitydesccol">${ability.desc}</span> `;
+		buf += `</a></li><li style="clear:both"></li></ul>`;
+		return buf;
+	}
+
 	getTemplate(template) {
 		if (!template || typeof template === 'string') {
 			let name = (template || '').trim();
