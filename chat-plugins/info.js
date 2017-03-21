57588c94ea896adb6dd31cf4d048a0317519e734
Spandamn <Spandamn>
2017-03-21 13:26:33 +0530

    Update info.js

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -343,7 +343,7 @@ exports.commands = {
 		if (newTargets && newTargets.length) {
 			for (let i = 0; i < newTargets.length; ++i) {
 				if (!newTargets[i].exactMatch && !i) {
-					buffer = "No Pok\u00e9mon, item, move, ability or nature named '" + target + "' was found. Showing the data of '" + newTargets[0].name + "' instead.\n";
+					buffer = "No Pok\u00e9mon, item, move, ability or nature named '" + sep[0] + "' was found. Showing the data of '" + newTargets[0].name + "' instead.\n";
 				}
 				if (newTargets[i].searchType === 'nature') {
 					let nature = Tools.getNature(newTargets[i].name);
@@ -359,6 +359,7 @@ exports.commands = {
 					let sType = newTargets[i].searchType.charAt(0).toUpperCase() + newTargets[i].searchType.substring(1, newTargets[i].searchType.length);
 					if (Chat[`getData${sType}HTML`]) {
 						let template = mod["get" + (sType === "Pokemon" ? "Template" : sType)](newTargets[i].name);
+						if (template.gen > mod.gen) return this.errorReply(`No Pok\u00e9mon, item, move, ability or nature named '${sep[0]}' was found in Gen ${mod.gen}.`);
 						buffer += `|raw|${Chat[`getData${sType}HTML`](template)}\n`;
 					} else {
 						buffer += '|c|~|/data-' + newTargets[i].searchType + ' ' + newTargets[i].name + '\n';
