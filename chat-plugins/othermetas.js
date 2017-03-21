7a9bde0e436af97358bb82d80f235678c4eb9b3e
Spandamn <Spandamn>
2017-03-21 12:24:04 +0530

    Remove /distor and /dgen

--- chat-plugins/othermetas.js
+++ chat-plugins/othermetas.js
@@ -400,44 +400,6 @@ exports.commands = {
 			this.sendReplyBox(text);
 		}
 	},
-	di: 'distor',
-	dataistor: 'distor',
-	distor: function(target, room, user) {
-        	 if (!this.runBroadcast()) return;
-                 if(!target || toId(target) === '') return this.sendReply("/distor: Shows the data for a Pokemon/Ability/Move, including ones from istor.");
-		let name = toId(target);
-		let abilistor = Tools.mod('istor').data.Abilities, movestor = Tools.mod('istor').data.Movedex, pokemen = Tools.mod('istor').data.Pokedex, itemstor = Tools.mod('istor').data.Items;
-		if(pokemen[name]) {
-			let baseStats = pokemen[name].baseStats;
-			let types = pokemen[name].types;
-			let type = '<span class="col typecol">';
-			for(let i = 0; i<types.length;i++) {
-				type = type+ '<img src="https://play.pokemonshowdown.com/sprites/types/'+types[i]+'.png" alt="'+types[i]+'" height="14" width="32">';
-			}
-			type = type+"</span>";
-			let ability = "";
-			let weight = pokemen[name].weightkg;
-			for(let i in pokemen[name].abilities) {
-				ability+=pokemen[name].abilities[i]+"/";
-			}
-			ability = ability.substring(0,ability.length-1);
-			let bst = baseStats['hp'] + baseStats['atk'] + baseStats['def'] + baseStats['spa'] + baseStats['spd'] + baseStats['spe'];
-			let text = "<b>Stats</b>: " + baseStats['hp'] + "/" + baseStats['atk'] + "/" + baseStats['def'] + "/" + baseStats['spa'] + "/" + baseStats['spd'] + "/" + baseStats['spe'] + "<br /><b>BST</b>:" + bst + "<br /><b>Type:</b> " + type + "<br /><b>Abilities</b>: " +ability+ "<br /><b>Weight</b>: "+weight+" kg";
-			return this.sendReplyBox(text);
-		}
-		else if(movestor[name] && (movestor.desc || movestor[name].shortDesc)) {
-			return this.sendReplyBox(`<ul class="utilichart"><li class="result"><span class="col movenamecol">${movestor[name].name}</span> <span class="col typecol"><img src="//play.pokemonshowdown.com/sprites/types/${(movestor[name].type)}.png" alt="${(movestor[name].type)}" height="14" width="32"><img src="//play.pokemonshowdown.com/sprites/categories/${(movestor[name].category)}.png" alt="${(movestor[name].category)}" height="14" width="32"></span> <span class="col labelcol"><em>Power</em><br>${(movestor[name].basePower)}</span> <span class="col widelabelcol"><em>Accuracy</em><br>${(movestor[name].accuracy)}%</span> <span class="col pplabelcol"><em>PP</em><br>${(movestor[name].pp)}</span> <span class="col movedesccol">${(movestor[name].shortDesc)}</span> </li><li style="clear:both"></li></ul><div class="chat"><font size="1"><font color="#686868">Priority:</font> ${(movestor[name].priority)}|<font color="#686868">Gen:</font> Istor |<font color="#686868"> Target:</font>${(movestor[name].target)}</div>`);
-		}
-		else if(abilistor[name] && (abilistor[name].desc || abilistor[name].shortDesc)) {
-			return this.sendReplyBox(`<b>${abilistor[name].name}</b>: ${(abilistor[name].desc || abilistor[name].shortDesc)}`);
-		}
-		else if(itemstor[name] && (itemstor[name].desc || itemstor[name].shortDesc)) {
-			return this.sendReplyBox(`<b>${itemstor[name].name}</b>: ${(itemstor[name].desc || itemstor[name].shortDesc)}`);
-		}
-		else 
-			return this.errorReply("Error: Pokemon/Ability/Move not found");
-		
-	},
         learnistor: function(target, room, user) {
                 if (!this.runBroadcast()) return;
 		let learnstor = Tools.mod('istor').data.Learnsets, movestor = Tools.mod('istor').data.Movedex, dexstor = Tools.mod('istor').data.Pokedex;
@@ -453,43 +415,6 @@ exports.commands = {
                 }
                 return this.sendReplyBox("In Istor, "+mon.species+' <font color="red"><u><b>can\'t<b><u></font> learn '+move.name);
         },
-	dgen: 'dnewgen',
-	dnewgen: function(target, room, user) {
-        	 if (!this.runBroadcast()) return;
-                 if(!target || toId(target) === '') return this.sendReply("/dgen: Shows the data for a Pokemon/Ability/Move/Item, including ones from Pokemon The New First Gen.");
-		let name = toId(target);
-		let abiliden = Tools.mod('thefirstnewgen').data.Abilities, moveden = Tools.mod('thefirstnewgen').data.Movedex, pokegen = Tools.mod('thefirstnewgen').data.Pokedex, itemgen = Tools.mod('thefirstnewgen').data.Items;
-		if(pokegen[name]) {
-			let baseStats = pokegen[name].baseStats;
-			let types = pokegen[name].types;
-			let type = '<span class="col typecol">';
-			for(let i = 0; i<types.length;i++) {
-				type = type+ '<img src="https://play.pokemonshowdown.com/sprites/types/'+types[i]+'.png" alt="'+types[i]+'" height="14" width="32">';
-			}
-			type = type+"</span>";
-			let ability = "";
-			let info = pokegen[name].gender;
-			let weight = pokegen[name].weightkg;
-			for(let i in pokegen[name].abilities) {
-				ability+=pokegen[name].abilities[i]+"/";
-			}
-			ability = ability.substring(0,ability.length-1);
-			let bst = baseStats['hp'] + baseStats['atk'] + baseStats['def'] + baseStats['spa'] + baseStats['spd'] + baseStats['spe'];
-			let text = ""+info+"</button><br><b>Stats</b>: " + baseStats['hp'] + "/" + baseStats['atk'] + "/" + baseStats['def'] + "/" + baseStats['spa'] + "/" + baseStats['spd'] + "/" + baseStats['spe'] + "<br /><b>BST</b>:" + bst + "<br /><b>Type:</b> " + type + "<br /><b>Abilities</b>: " +ability+ "<br /><b>Weight</b>: "+weight+" kg";
-			return this.sendReplyBox(text);
-		}
-		else if(moveden[name]) {
-			return this.sendReplyBox(`<ul class="utilichart"><li class="result"><span class="col movenamecol">${moveden[name].name}</span> <span class="col typecol"><img src="//play.pokemonshowdown.com/sprites/types/${(moveden[name].type)}.png" alt="${(moveden[name].type)}" height="14" width="32"><img src="//play.pokemonshowdown.com/sprites/categories/${(moveden[name].category)}.png" alt="${(moveden[name].category)}" height="14" width="32"></span> <span class="col labelcol"><em>Power</em><br>${(moveden[name].basePower)}</span> <span class="col widelabelcol"><em>Accuracy</em><br>${(moveden[name].accuracy)}%</span> <span class="col pplabelcol"><em>PP</em><br>${(moveden[name].pp)}</span> <span class="col movedesccol">${(moveden[name].shortDesc)}</span> </li><li style="clear:both"></li></ul><div class="chat"><font size="1"><font color="#686868">Priority:</font> ${(moveden[name].priority)}|<font color="#686868">Gen:</font> New First Gen |<font color="#686868"> Target:</font>${(moveden[name].target)}</div>`);
-		}
-		else if(abiliden[name]) {
-			return this.sendReplyBox(`<b>${abiliden[name].name}</b>: ${(abiliden[name].shortDesc)}`);
-		}
-		else if(itemgen[name]) {
-			return this.sendReplyBox(`<b>${itemgen[name].name}</b>: ${(itemgen[name].desc || itemgen[name].shortDesc)}`);
-		}
-		else 
-			return this.errorReply("Error: Pokemon/Ability/Move/Item not found");
-	},
 	
 	'bnb' : 'badnboosted',
 	badnboosted : function (target, room, user) {
