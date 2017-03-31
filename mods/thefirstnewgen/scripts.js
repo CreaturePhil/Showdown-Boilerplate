b08c3f12d655c7f9013e3527604303371409ce8f
Spandamn <Spandamn>
2017-03-31 15:25:36 +0530

    TFNG

--- mods/thefirstnewgen/scripts.js
+++ mods/thefirstnewgen/scripts.js
@@ -2,7 +2,7 @@
 
 exports.BattleScripts = {
 	init: function() {
-		let fData = Object.assign({}, Tools.mod('base').data.FormatsData);
+		let fData = Object.assign({}, Tools.data.FormatsData);
 		Object.values(this.data.Pokedex).forEach(pokemon => {
 			if (pokemon.num >= 8000) fData[toId(pokemon.name)].tier = "TFNG";
 		});
